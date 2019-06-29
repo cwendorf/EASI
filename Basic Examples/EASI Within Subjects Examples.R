123619d22c24cf290e6e66fe53ab38552a18b311
cwendorf <45096238+cwendorf@users.noreply.github.com>
2019-06-29 16:54:45 -0500

    Moved to New Folders

--- Basic Examples/EASI Within Subjects Examples.R
+++ Basic Examples/EASI Within Subjects Examples.R
@@ -13,42 +13,47 @@ mydata
 
 # Analysis of a Single Variable
 
-estVars(Time1)
-testVars(Time1)
+estimateVariables(Time1)
+testVariables(Time1)
+effectVariables(Time1)
 
-estVars(Time1,conf.level=.99)
-testVars(Time1,mu=5)
+estimateVariables(Time1,conf.level=.99)
+testVariables(Time1,mu=5)
+effectVariables(Time1,mu=5)
 
 # Analyses of Multiple Variables
 
-estVars(Time1,Time2)
-plotVars(t1,t2)
+estimateVariables(Time1,Time2,Time3)
+plotVariables(Time1,Time2,Time3)
+testVariables(Time1,Time2,Time3)
+effectVariables(Time1,Time2,Time3)
 
-nhstVars(Time1,Time2)
-testVars(Time1,Time2)
-testVars(Time1,Time2,mu=6)
-
-estVars(Time1,Time2,conf.level=.99)
-plotVars(Time1,Time2,conf.level=.99)
-
-estVars(Time1,Time2,Time3)
-plotVars(Time1,Time2,Time3)
-testVars(Time1,Time2,Time3,mu=6)
-plotVars(Time1,Time2,Time3,mu=6)
-
-estVars(Time1,Time2,Time3,conf.level=.99)
-plotVars(Time1,Time2,Time3,conf.level=.99)
+estimateVariables(Time1,Time2,Time3,conf.level=.99)
+plotVariables(Time1,Time2,Time3,conf.level=.99,mu=6)
+testVariables(Time1,Time2,Time3,mu=6)
+effectVariables(Time1,Time2,Time3,mu=6,conf.level=.99)
 
 # Analyses of Variable Differences
 
-estVars(Time1,Time2)
-estVarDiff(Time1,Time2)
-plotVarDiff(Time1,Time2)
-testVarDiff(Time1,Time2)
+estimateVariables(Time1,Time2)
+estimateVariableDiff(Time1,Time2)
+plotVariableDiff(Time1,Time2)
+testVariableDiff(Time1,Time2)
 
-estVarDiff(Time1,Time2,conf.level=.99)
-plotVarDiff(Time1,Time2,conf.level=.99)
-testVarDiff(Time1,Time2,mu=-2)
+estimateVariableDiff(Time1,Time2,conf.level=.99)
+plotVariableDiff(Time1,Time2,conf.level=.99)
+testVariableDiff(Time1,Time2,mu=-2)
+
+estimateVariables(Time3,Time1)
+estimateVariableDiff(Time3,Time1)
+plotVariableDiff(Time3,Time1)
+testVariableDiff(Time3,Time1)
+
+Time1a2=(Time1+Time2)/2
+estimateVariables(Time3,Time1a2)
+estimateVariableDiff(Time3,Time1a2)
+plotVariableDiff(Time3,Time1a2)
+testVariableDiff(Time3,Time1a2)
 
 
 
