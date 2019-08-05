589fbe57a11e2ab0877ca2e541ba834b02445bc2
cwendorf <45096238+cwendorf@users.noreply.github.com>
2019-08-05 05:48:52 -0500

    Updated examples

--- C-ITNSExamples/DonohueSyntax.R
+++ C-ITNSExamples/DonohueSyntax.R
@@ -1,15 +1,7 @@
 # ESTIMATION APPROACH TO STATISTICAL INFERENCE (EASI)
 ## WITHIN-SUBJECTS EXAMPLE WITH DONOHUE DATA
 
-### Input Data
-
-Pretest=c(7,11,11,13,15,17,18,18,19,19,19,19,21,22,25,25,25,26,27,29)
-Posttest=c(16,20,25,22,28,12,16,26,21,22,26,30,23,27,23,27,28,24,28,28)
-Followup=c(16,18,23,21,27,21,18,21,24,21,27,28,18,27,23,28,31,18,31,27)
-Donohue=data.frame(Pretest,Posttest,Followup)
-Donohue
-
-### Alternatives for Reading Data Instead
+### Read Data from Files
 
 Donohue=read.table("DonohueData.csv",header=T,sep=",")
 Donohue=read.csv("DonohueData.csv")
@@ -18,6 +10,14 @@ Donohue
 
 load(file="DonohueData.rda")
 attach(Donohue)
+Donohue
+
+### Input Data Instead
+
+Pretest=c(7,11,11,13,15,17,18,18,19,19,19,19,21,22,25,25,25,26,27,29)
+Posttest=c(16,20,25,22,28,12,16,26,21,22,26,30,23,27,23,27,28,24,28,28)
+Followup=c(16,18,23,21,27,21,18,21,24,21,27,28,18,27,23,28,31,18,31,27)
+Donohue=data.frame(Pretest,Posttest,Followup)
 Donohue
 
 ### Analyses of the Different Variables
