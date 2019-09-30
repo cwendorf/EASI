7f0ea43fd36920f466edafdb63db770a0ba87b8f
cwendorf <45096238+cwendorf@users.noreply.github.com>
2019-09-29 19:40:24 -0500

    Added ROPE to plots

--- A-Functions/plotExtensions.R
+++ A-Functions/plotExtensions.R
@@ -8,18 +8,18 @@
 plotPairwise <- function(...) 
   UseMethod("plotPairwise")
 
-plotPairwise.default <- function(...,mu=NULL) {
+plotPairwise.default <- plotPairwise.bss <- plotPairwise.wss <- function(...,mu=NULL,rope=NULL) {
   main="Confidence Intervals for the Pairwise Comparisons"
   ylab="Mean Difference"
   xlab="Pairwise Comparisons"
   results <- ciPairwise(...)[,c(1,4,5)]
-  cipMeans(results,main,ylab,xlab,mu) 
+  cipMeans(results,main,ylab,xlab,mu,rope) 
 }
 
-plotPairwise.formula <- function(formula,conf.level=.95,mu=NA,...) {
+plotPairwise.formula <- function(formula,conf.level=.95,mu=NA,rope=NULL,...) {
   main="Confidence Intervals for the Pairwise Comparisons"
   ylab="Mean Difference"
   xlab="Pairwise Comparisons"
   results <- ciPairwise(formula,...)[,c(1,4,5)]
-  cipMeans(results,main,ylab,xlab,mu) 
+  cipMeans(results,main,ylab,xlab,mu,rope) 
 }
