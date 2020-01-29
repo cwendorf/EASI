8c56393fbb59dd72455cb4a6d983c4f1d4247953
cwendorf <45096238+cwendorf@users.noreply.github.com>
2020-01-29 05:56:58 -0600

    Updated ROPE

--- A-Functions/EASI-Analyze-Extension.R
+++ A-Functions/EASI-Analyze-Extension.R
@@ -6,32 +6,32 @@
 
 ### Meta-Wrappers for All Functions
 
-analyzeMeans <- function(...){
+analyzeMeans <- function(...,conf.level=.95,mu=0,rope=NULL){
   Output <- list(
     title="ANALYSIS OF MEANS",
-    estimateMeans=format(as.data.frame(ciMeans(...)),trim=T,nsmall=3),
-    testMeans=format(as.data.frame(nhstMeans(...)),trim=T,nsmall=3),
-    standardizeMeans=format(as.data.frame(smdMeans(...)),trim=T,nsmall=3))
-  plotMeans(...)  
+    estimateMeans=format(as.data.frame(ciMeans(...,conf.level=conf.level)),trim=T,nsmall=3),
+    testMeans=format(as.data.frame(nhstMeans(...,mu=mu)),trim=T,nsmall=3),
+    standardizeMeans=format(as.data.frame(smdMeans(...,mu=mu,conf.level=conf.level)),trim=T,nsmall=3))
+  plotMeans(...,conf.level=conf.level,mu=mu,rope=rope)  
   return(Output)
 }
 
-analyzeDifference <- function(...){
+analyzeDifference <- function(...,conf.level=.95,mu=0,rope=NULL){
   Output <- list(
     title="ANALYSIS OF A DIFFERENCE",
-    estimateDifference=format(as.data.frame(ciDifference(...)),trim=T,nsmall=3),
-    testDifference=format(as.data.frame(nhstDifference(...)),trim=T,nsmall=3),
-    standardizeDifference=format(as.data.frame(smdDifference(...)),trim=T,nsmall=3))
-  plotDifference(...)  
+    estimateDifference=format(as.data.frame(ciDifference(...,conf.level=conf.level)),trim=T,nsmall=3),
+    testDifference=format(as.data.frame(nhstDifference(...,mu=mu)),trim=T,nsmall=3),
+    standardizeDifference=format(as.data.frame(smdDifference(...,mu=mu,conf.level=conf.level)),trim=T,nsmall=3))
+  plotDifference(...,conf.level=conf.level,mu=mu,rope=rope)  
   return(Output)
 }
 
-analyzeContrast <- function(...){
+analyzeContrast <- function(...,conf.level=.95,mu=0,rope=NULL){
   Output <- list(
     title="ANALYSIS OF A CONTRAST",
-    estimateContrast=format(as.data.frame(ciContrast(...)),trim=T,nsmall=3),
-    testContrast=format(as.data.frame(nhstContrast(...)),trim=T,nsmall=3),
-    standardizeContrast=format(as.data.frame(smdContrast(...)),trim=T,nsmall=3))
-  plotContrast(...)  
+    estimateContrast=format(as.data.frame(ciContrast(...,conf.level=conf.level)),trim=T,nsmall=3),
+    testContrast=format(as.data.frame(nhstContrast(...,mu=mu)),trim=T,nsmall=3),
+    standardizeContrast=format(as.data.frame(smdContrast(...,mu=mu,conf.level=conf.level)),trim=T,nsmall=3))
+  plotContrast(...,conf.level=conf.level,mu=mu,rope=rope)  
   return(Output)
 }
