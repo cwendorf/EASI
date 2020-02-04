cac2bbd3004669c5430ef7f89ed7b279a3fd40d3
cwendorf <45096238+cwendorf@users.noreply.github.com>
2020-02-03 18:56:54 -0600

    Updated within-subjects plots

--- A-Functions/EASI-Functions.R
+++ A-Functions/EASI-Functions.R
@@ -285,7 +285,7 @@ plotData.formula <- function(formula,add=FALSE,method="jitter",col="gray60",pch=
   stripchart(formula,add=add,xlim=xlimrange,ylim=ylimrange,at=mn:mx,vertical=TRUE,method=method,main=main,jitter=0.08,col=col,pch=pch,cex.lab=1.3,...)
 }
 
-### Add Data to Plot
+#### Add Data to Plot
 
 addData <- function(...) 
   UseMethod("addData")
@@ -477,6 +477,164 @@ plotContrast.formula <- function(formula,contrast,conf.level=.95,rope=NULL,label
   DescStats <- descData(formula)
   class(DescStats) <- "bss"
   plotContrast(DescStats,contrast=contrast,conf.level=conf.level,rope=rope,labels=labels,values=values)
+}
+
+### Null Hypothesis Significance Test Functions
+
+#### NHST Function for Means
+
+nhstMeans <- function(...) 
+  UseMethod("nhstMeans")
+  
+nhstMeans.wss <- nhstMeans.bss <- function(DescStats,mu=0,...) {
+  N <- DescStats[,"N"]
+  M <- DescStats[,"M"]
+  SE <- DescStats[,"SD"]/sqrt(N)
+  Diff <- M-mu
+  t <- Diff/SE
+  df <- N-1
+  p <- 2*(1 - pt(abs(t),df))
+  results <- round(cbind(Diff=Diff,SE=SE,t=t,df=df,p=p),3)
+  rownames(results) <- rownames(DescStats)  
+  return(results)
+}
+
+nhstMeans.default <- function(...,mu=0) {
+  DescStats <- descData(...)
+  class(DescStats) <- "wss"
+  results <- nhstMeans(DescStats,mu=mu)
+  return(results)
+}
+
+nhstMeans.formula <- function(formula,mu=0,...) {
+  DescStats <- descData(formula)
+  class(DescStats) <- "bss"
+  results <- nhstMeans(DescStats,mu=mu)
+  return(results)
+}
+
+testMeans <- function(...) {
+  cat("\nHYPOTHESIS TESTS FOR THE MEANS\n\n")
+  print(format(as.data.frame(nhstMeans(...)),trim=T,nsmall=3))
+  cat("\n")
+}
+
+##### NHST Function for Mean Differences/Comparisons
+
+nhstDifference <- function(...) 
+  UseMethod("nhstDifference")
+  
+nhstDifference.wss <- function(CompStats,CorrStats,mu=0,...) {
+  CompStats <- CompStats[1:2,]
+  N <- CompStats[,"N"]
+  M <- CompStats[,"M"]
+  SD <- CompStats[,"SD"]
+  rn <- rownames(CompStats)
+  R <- CorrStats[rn[1],rn[2]]
+  MD <- M[1]-M[2]-mu
+  SE <- SD/sqrt(N)
+  SE <- sqrt(SE[1]^2+SE[2]^2-2*R*SE[1]*SE[2])
+  df <- min(N)-1
+  t <- MD/SE
+  p <- 2*(1 - pt(abs(t),df))
+  results <- round(cbind(Diff=MD,SE=SE,t=t,df=df,p=p),3)  
+  rownames(results) <- c("Comparison")
+  return(results)
+}
+
+nhstDifference.bss <- function(CompStats,mu=0,...) {
+  CompStats <- CompStats[1:2,]
+  N <- CompStats[,"N"]
+  M <- CompStats[,"M"]
+  SD <- CompStats[,"SD"]
+  MD <- M[1]-M[2]-mu
+  SE <- sqrt( (SD[1]^2/N[1]) + (SD[2]^2/N[2]) )
+  df <- ((SD[1]^2/N[1] + SD[2]^2/N[2])^2 )/( (SD[1]^2/N[1])^2/(N[1]-1) + (SD[2]^2/N[2])^2/(N[2]-1) )
+  t <- MD/SE
+  p <- 2*(1 - pt(abs(t),df))
+  results <- round(cbind(Diff=MD,SE=SE,t=t,df=df,p=p),3)
+  rownames(results) <- c("Comparison")
+  return(results)
+}
+
+nhstDifference.default <- function(...,mu=0) {
+  CompStats <- descData(...)
+  class(CompStats) <- "wss"
+  CorrStats <- corrData(...)
+  results <- nhstDifference(CompStats,CorrStats,mu=mu)
+  return(results)
+}
+
+nhstDifference.formula <- function(formula,mu=0,...) {
+  CompStats <- descData(formula)
+  class(CompStats) <- "bss"
+  results <- nhstDifference(CompStats,mu=mu)
+  return(results)
+}
+
+testDifference <- function(...) {
+  cat("\nHYPOTHESIS TEST FOR THE COMPARISON\n\n")
+  print(format(as.data.frame(nhstDifference(...)),trim=T,nsmall=3))
+  cat("\n")
+}
+
+#### NHST Function for Mean Contrasts
+
+nhstContrast <- function(...) 
+  UseMethod("nhstContrast")
+  
+nhstContrast.bss <- function(DescStats,contrast,mu=0,...) {
+  N=DescStats[,"N"]
+  M=DescStats[,"M"]
+  SD=DescStats[,"SD"]
+  Est <- (t(contrast)%*%M)-mu
+  k <- length(M)
+  v <- diag(SD^2)%*%(solve(diag(N)))
+  SE <- sqrt(t(contrast)%*%v%*%contrast)
+  df <- (SE^4)/sum(((contrast^4)*(SD^4)/(N^2*(N-1))))
+  t <- Est/SE
+  p <- 2*(1 - pt(abs(t),df))
+  results <- t(c(Est,SE,t,df,p))
+  colnames(results) <- c("Est","SE","t","df","p")
+  rownames(results) <- c("Contrast")
+  return(round(results,3))
+}
+
+nhstContrast.wss <- function(DescStats,CorrStats,contrast,mu=0,conf.level=.95,...) {
+  N <- min(DescStats[,"N"])
+  M <- DescStats[,"M"]
+  SD <- DescStats[,"SD"]
+  Est <- (t(contrast)%*%M)-mu
+  covstats <- cor2cov(CorrStats,SD)
+  SE <- sqrt(t(contrast)%*%covstats%*%contrast/N)
+  t <- Est/SE
+  df <- N-1
+  p <- 2*(1 - pt(abs(t),df))
+  results <- t(c(Est,SE,t,df,p))
+  colnames(results) <- c("Est","SE","t","df","p")
+  rownames(results) <- c("Contrast")
+  return(round(results,3))
+}
+
+nhstContrast.default <- function(...,contrast,mu=0,conf.level=.95) {
+  DescStats <- descData(...)
+  class(DescStats) <- "wss"
+  CorrStats <- corrData(...)
+  results <- nhstContrast(DescStats,CorrStats,contrast,mu=mu)
+  return(results)
+}
+
+nhstContrast.formula <- function(formula,contrast,mu=0,...) {
+  DescStats <- descData(formula)
+  class(DescStats) <- "bss"
+  results <- nhstContrast(DescStats,contrast,mu=mu)
+  return(results)
+}
+
+testContrast<-function(...) {
+  cat("\nHYPOTHESIS TEST FOR THE CONTRAST\n\n")
+  print(format(as.data.frame(nhstContrast(...)),trim=T,nsmall=3))
+  cat("\n")  
 }
 
 ### Standardized Mean Difference Functions
@@ -681,160 +839,3 @@ standardizeContrast <- function(...) {
   cat("\n")  
 }
 
-### Null Hypothesis Significance Test Functions
-
-#### NHST Function for Means
-
-nhstMeans <- function(...) 
-  UseMethod("nhstMeans")
-  
-nhstMeans.wss <- nhstMeans.bss <- function(DescStats,mu=0,...) {
-  N <- DescStats[,"N"]
-  M <- DescStats[,"M"]
-  SE <- DescStats[,"SD"]/sqrt(N)
-  Diff <- M-mu
-  t <- Diff/SE
-  df <- N-1
-  p <- 2*(1 - pt(abs(t),df))
-  results <- round(cbind(Diff=Diff,SE=SE,t=t,df=df,p=p),3)
-  rownames(results) <- rownames(DescStats)  
-  return(results)
-}
-
-nhstMeans.default <- function(...,mu=0) {
-  DescStats <- descData(...)
-  class(DescStats) <- "wss"
-  results <- nhstMeans(DescStats,mu=mu)
-  return(results)
-}
-
-nhstMeans.formula <- function(formula,mu=0,...) {
-  DescStats <- descData(formula)
-  class(DescStats) <- "bss"
-  results <- nhstMeans(DescStats,mu=mu)
-  return(results)
-}
-
-testMeans <- function(...) {
-  cat("\nHYPOTHESIS TESTS FOR THE MEANS\n\n")
-  print(format(as.data.frame(nhstMeans(...)),trim=T,nsmall=3))
-  cat("\n")
-}
-
-##### NHST Function for Mean Differences/Comparisons
-
-nhstDifference <- function(...) 
-  UseMethod("nhstDifference")
-  
-nhstDifference.wss <- function(CompStats,CorrStats,mu=0,...) {
-  CompStats <- CompStats[1:2,]
-  N <- CompStats[,"N"]
-  M <- CompStats[,"M"]
-  SD <- CompStats[,"SD"]
-  rn <- rownames(CompStats)
-  R <- CorrStats[rn[1],rn[2]]
-  MD <- M[1]-M[2]-mu
-  SE <- SD/sqrt(N)
-  SE <- sqrt(SE[1]^2+SE[2]^2-2*R*SE[1]*SE[2])
-  df <- min(N)-1
-  t <- MD/SE
-  p <- 2*(1 - pt(abs(t),df))
-  results <- round(cbind(Diff=MD,SE=SE,t=t,df=df,p=p),3)  
-  rownames(results) <- c("Comparison")
-  return(results)
-}
-
-nhstDifference.bss <- function(CompStats,mu=0,...) {
-  CompStats <- CompStats[1:2,]
-  N <- CompStats[,"N"]
-  M <- CompStats[,"M"]
-  SD <- CompStats[,"SD"]
-  MD <- M[1]-M[2]-mu
-  SE <- sqrt( (SD[1]^2/N[1]) + (SD[2]^2/N[2]) )
-  df <- ((SD[1]^2/N[1] + SD[2]^2/N[2])^2 )/( (SD[1]^2/N[1])^2/(N[1]-1) + (SD[2]^2/N[2])^2/(N[2]-1) )
-  t <- MD/SE
-  p <- 2*(1 - pt(abs(t),df))
-  results <- round(cbind(Diff=MD,SE=SE,t=t,df=df,p=p),3)
-  rownames(results) <- c("Comparison")
-  return(results)
-}
-
-nhstDifference.default <- function(...,mu=0) {
-  CompStats <- descData(...)
-  class(CompStats) <- "wss"
-  CorrStats <- corrData(...)
-  results <- nhstDifference(CompStats,CorrStats,mu=mu)
-  return(results)
-}
-
-nhstDifference.formula <- function(formula,mu=0,...) {
-  CompStats <- descData(formula)
-  class(CompStats) <- "bss"
-  results <- nhstDifference(CompStats,mu=mu)
-  return(results)
-}
-
-testDifference <- function(...) {
-  cat("\nHYPOTHESIS TEST FOR THE COMPARISON\n\n")
-  print(format(as.data.frame(nhstDifference(...)),trim=T,nsmall=3))
-  cat("\n")
-}
-
-#### NHST Function for Mean Contrasts
-
-nhstContrast <- function(...) 
-  UseMethod("nhstContrast")
-  
-nhstContrast.bss <- function(DescStats,contrast,mu=0,...) {
-  N=DescStats[,"N"]
-  M=DescStats[,"M"]
-  SD=DescStats[,"SD"]
-  Est <- (t(contrast)%*%M)-mu
-  k <- length(M)
-  v <- diag(SD^2)%*%(solve(diag(N)))
-  SE <- sqrt(t(contrast)%*%v%*%contrast)
-  df <- (SE^4)/sum(((contrast^4)*(SD^4)/(N^2*(N-1))))
-  t <- Est/SE
-  p <- 2*(1 - pt(abs(t),df))
-  results <- t(c(Est,SE,t,df,p))
-  colnames(results) <- c("Est","SE","t","df","p")
-  rownames(results) <- c("Contrast")
-  return(round(results,3))
-}
-
-nhstContrast.wss <- function(DescStats,CorrStats,contrast,mu=0,conf.level=.95,...) {
-  N <- min(DescStats[,"N"])
-  M <- DescStats[,"M"]
-  SD <- DescStats[,"SD"]
-  Est <- (t(contrast)%*%M)-mu
-  covstats <- cor2cov(CorrStats,SD)
-  SE <- sqrt(t(contrast)%*%covstats%*%contrast/N)
-  t <- Est/SE
-  df <- N-1
-  p <- 2*(1 - pt(abs(t),df))
-  results <- t(c(Est,SE,t,df,p))
-  colnames(results) <- c("Est","SE","t","df","p")
-  rownames(results) <- c("Contrast")
-  return(round(results,3))
-}
-
-nhstContrast.default <- function(...,contrast,mu=0,conf.level=.95) {
-  DescStats <- descData(...)
-  class(DescStats) <- "wss"
-  CorrStats <- corrData(...)
-  results <- nhstContrast(DescStats,CorrStats,contrast,mu=mu)
-  return(results)
-}
-
-nhstContrast.formula <- function(formula,contrast,mu=0,...) {
-  DescStats <- descData(formula)
-  class(DescStats) <- "bss"
-  results <- nhstContrast(DescStats,contrast,mu=mu)
-  return(results)
-}
-
-testContrast<-function(...) {
-  cat("\nHYPOTHESIS TEST FOR THE CONTRAST\n\n")
-  print(format(as.data.frame(nhstContrast(...)),trim=T,nsmall=3))
-  cat("\n")  
-}
