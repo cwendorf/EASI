276202004f04e6e82c5120e43dc15b8b27209b34
cwendorf <45096238+cwendorf@users.noreply.github.com>
2020-02-21 15:36:01 -0600

    Update omnibusExtension.R

--- R/omnibusExtension.R
+++ R/omnibusExtension.R
@@ -128,3 +128,76 @@ testOmnibus <- function(...,digits=3) {
   print(formatFrame(nhstOmnibus(...),digits=digits))
   cat("\n")
 }
+
+### Omnibus Effect Size
+
+ncpF <- function(x,df1,df2,prob,interval=c(0,10000),my.tol=0.000001) {
+  temp <- function(ncp) pf(x,df1,df2,ncp) - prob
+  return(uniroot(temp, interval, tol = my.tol)$root)
+}
+
+povOmnibus <- function(...) 
+  UseMethod("povOmnibus")
+
+povOmnibus.bss <- function(DescStats,conf.level=.90) {
+  temptab <- descOmnibus(DescStats)
+  SSb <- temptab["Between","SS"]
+  SSw <- temptab["Within","SS"]
+  SSt <- SSb + SSw
+  dfb <- temptab["Between","df"]
+  dfw <- temptab["Within","df"] 
+  dft <- dfb + dfw
+  F <- (SSb/dfb)/(SSw/dfw) 
+  etasq <- SSb / SSt
+  delta.lower <- delta.upper <- numeric(length(etasq))
+  delta.lower <- try(ncpF(F,dfb,dfw,prob=(1+conf.level)/2),silent=TRUE)
+  delta.upper <- try(ncpF(F,dfb,dfw,prob=(1-conf.level)/2),silent=TRUE)
+  if(is.character(delta.lower)) {delta.lower <- 0}
+  etasq.lower <- delta.lower / (delta.lower + dfb + dfw + 1)
+  etasq.upper <- delta.upper / (delta.upper + dfb + dfw + 1)
+  results <- cbind(EST=etasq,LL=etasq.lower,UL=etasq.upper)
+  rownames(results) <- "Omnibus"
+  return(results)
+}
+
+povOmnibus.wss <- function(DescStats,CorrStats,conf.level=.90) {
+  temptab <- descOmnibus(DescStats,CorrStats)
+  SSf <- temptab["Factor","SS"]
+  SSe <- temptab["Error","SS"]
+  SSt <- SSf + SSe
+  dff <- temptab["Factor","df"]
+  dfe <- temptab["Error","df"] 
+  dft <- dff + dfe
+  F <- (SSf/dff)/(SSe/dfe)
+  etasq <- SSf / SSt
+  delta.lower <- delta.upper <- numeric(length(etasq))
+  delta.lower <- try(ncpF(F,dff,dfe,prob=(1+conf.level)/2),silent=TRUE)
+  delta.upper <- try(ncpF(F,dff,dfe,prob=(1-conf.level)/2),silent=TRUE)
+  if(is.character(delta.lower)) {delta.lower <- 0}
+  etasq.lower <- delta.lower / (delta.lower + dff + dfe + 1)
+  etasq.upper <- delta.upper / (delta.upper + dff + dfe + 1)
+  results <- cbind(EST=etasq,LL=etasq.lower,UL=etasq.upper)
+  rownames(results) <- "Omnibus"
+  return(results)
+}
+
+povOmnibus.default <- function(...,mu=0,conf.level=.90,rope=NULL) {
+  DescStats <- descData(...)
+  class(DescStats) <- "wss"
+  CorrStats <- corrData(...)  
+  results <- povOmnibus(DescStats,CorrStats)
+  return(results)
+}
+
+povOmnibus.formula <- function(formula,mu=0,conf.level=.90,rope=NULL) {
+  DescStats <- descData(formula)
+  class(DescStats) <- "bss"
+  results <- povOmnibus(DescStats,conf.level)
+  return(results)
+}
+
+effectOmnibus <- function(...,digits=3) {
+  cat("\nPROPORTION OF VARIANCE FOR BY THE OMNIBUS EFFECT\n\n")
+  print(formatFrame(povOmnibus(...),digits=digits))
+  cat("\n")
+}
