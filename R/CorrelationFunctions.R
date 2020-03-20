cf6c7bd451c7c87e8af1ee003aff233bfa19535e
cwendorf <45096238+cwendorf@users.noreply.github.com>
2020-03-20 15:15:25 -0500

    Added confidence intervals for correlations

--- R/CorrelationFunctions.R
+++ R/CorrelationFunctions.R
@@ -64,39 +64,92 @@ fillCorrelation <- fillCorrelations <- fillCorrMatrix <- function(mat) {
 
 ### Confidence Interval Functions
 
-#### CI Function for a Correlation
+#### Confidence Intervals for Correlations
 
-ciCorrelation <- function(...) 
-  UseMethod("ciCorrelation")
+ciCorrelations <- function(...) 
+  UseMethod("ciCorrelations")
 
-ciCorrelation.wss <- function(CompStats,CorrStats,mu=0,conf.level=.95,rope=NULL,...) {
-  CompStats <- CompStats[1:2,]
-  N <- min(CompStats[,"N"])
-  rn <- rownames(CompStats)  
-  R <- CorrStats[rn[1],rn[2]]
-  z <- qnorm((1+conf.level)/2)
-  SE <- sqrt(1/((N-3)))
-  zR <- log((1+R)/(1-R))/2
-  LL0 <- zR - z*SE
-  UL0 <- zR + z*SE
-  LL <- (exp(2*LL0)-1)/(exp(2*LL0)+1)
-  UL <- (exp(2*UL0)-1)/(exp(2*UL0)+1)
-  results <- data.frame(R=R,SE=SE,LL=LL,UL=UL)
-  rownames(results) <- "Correlation"  
+ciCorrelations.wss <- function(SumStats,CorrStats,conf.level=.95,...){
+  N <- SumStats[,"N"]
+  rn <- rownames(SumStats)
+  nr <- nrow(SumStats)
+  ncomp <- (nr)*(nr-1)/2
+  results <- data.frame(matrix(ncol=4,nrow=ncomp))
+  colnames(results) <- c("R","SE","LL","UL")
+  comp <- 1
+  for( i in 1:(nr-1) ){
+  for( j in (i+1):nr ){
+    rownames(results)[comp] <- paste(rn[i],"&",rn[j])
+    n <- min(N[rn[j]],N[rn[i]])
+    R <- CorrStats[rn[i],rn[j]]  
+    z <- qnorm((1+conf.level)/2)
+    SE <- sqrt(1/((n-3)))
+    zR <- log((1+R)/(1-R))/2
+    LL0 <- zR - z*SE
+    UL0 <- zR + z*SE
+    LL <- (exp(2*LL0)-1)/(exp(2*LL0)+1)
+    UL <- (exp(2*UL0)-1)/(exp(2*UL0)+1)
+    results[comp,] <- c(R,SE,LL,UL)
+   	comp <- comp+1
+  }
+  }
+  return(round(results,3))
+}
+
+ciCorrelations.default <- function(...,conf.level=.95){
+  SumStats <- descMeans(...)
+  class(SumStats) <- "wss"
+  CorrStats <- descCorrelation(...)
+  results <- ciCorrelations(SumStats,CorrStats,conf.level=conf.level)
   return(results)
 }
 
-ciCorrelation.default <- function(...,mu=0,conf.level=.95,rope=NULL) {
-  CompStats <- descMeans(...)
-  class(CompStats) <- "wss"
-  CorrStats <- descCorrelation(...)
-  results <- ciCorrelation(CompStats,CorrStats,conf.level=conf.level)
+estimateCorrelations <- function(...,conf.level=.95,main=NULL,digits=3) {
+  results <- formatList(list(ciCorrelations(...,conf.level=conf.level)),digits=digits)
+  if(is.null(main)) {names(results) <- "Confidence Intervals for the Correlations"} else {names(results) <- main}
   return(results)
 }
 
 estimateCorrelation <- function(...,conf.level=.95,main=NULL,digits=3) {
-  results <- formatList(list(ciCorrelation(...,conf.level=conf.level)),digits=digits)
+  results <- formatList(list(ciCorrelations(...,conf.level=conf.level)),digits=digits)
   if(is.null(main)) {names(results) <- "Confidence Interval for the Correlation"} else {names(results) <- main}
   return(results)
+}
+
+### Confidence Interval Plot Functions
+
+#### Basic Confidence Interval Plot Functions
+
+cipCorrelations <- function(results,main,ylab,xlab,mu,rope,values,digits) {
+  plot(results[,"R"],xaxs="i",yaxs="i",xaxt="n",xlim=c(.5,nrow(results)+.5),ylim=c(-1.1,1.1),xlab=xlab,cex.lab=1.3,ylab=ylab,main=main,las=1,cex=1.5,pch=16,bty="l")
+  axis(1,1:nrow(results),row.names(results))
+  results <- formatFrame(results,digits=digits)
+  for (i in 1:nrow(results)) lines(x=c(i,i),y=c(results[,"LL"][i],results[,"UL"][i]),lwd=2)
+  if(values) {
+  for (i in 1:nrow(results)) text(i,as.numeric(results[,"R"][i]),results[,"R"][i],cex=.8,pos=2,offset=.5,font=2)
+  for (i in 1:nrow(results)) text(i,as.numeric(results[,"LL"][i]),results[,"LL"][i],cex=.8,pos=2,offset=.5)  
+  for (i in 1:nrow(results)) text(i,as.numeric(results[,"UL"][i]),results[,"UL"][i],cex=.8,pos=2,offset=.5)}
+  if(!is.null(mu)) {abline(h=mu,lty=2)}
+  if(!is.null(rope)) {rect(0,rope[1],nrow(results)+1,rope[2],col=rgb(.5,.5,.5,.07),border=NA)}   
+}
+
+#### Plot Function for Confidence Intervals of the Correlations
+
+plotCorrelations <- plotCorrelation <- function(...) 
+  UseMethod("plotCorrelations")
+
+plotCorrelations.wss <- function(CompStats,CorrStats,main=NULL,ylab="Outcome",xlab="",conf.level=.95,mu=NULL,rope=NULL,values=TRUE,digits=3,...) {
+  results <- ciCorrelations(CompStats,CorrStats,conf.level=conf.level)
+  if(is.null(main)) {
+    if(nrow(results)>1) {main="Confidence Intervals for the Correlations"} else {main="Confidence Interval for the Correlation"}
+    }
+  cipCorrelations(results,main,ylab=ylab,xlab=xlab,mu=mu,rope=rope,values=values,digits=digits)
+}
+
+plotCorrelations.default <- function(...,main=NULL,ylab="Outcome",xlab="",conf.level=.95,rope=NULL,labels=NULL,values=TRUE,digits=3) {
+  CompStats <- descMeans(...)
+  class(CompStats) <- "wss"
+  CorrStats <- descCorrelation(...)
+  plotCorrelations(CompStats,CorrStats,main=main,ylab=ylab,xlab=xlab,conf.level=conf.level,rope=rope,labels=labels,values=values,digits=digits)
 }
 
