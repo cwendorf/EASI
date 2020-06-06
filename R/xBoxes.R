25c4af4ef1462d57f57224265acfe54dd0eac69f
cwendorf <45096238+cwendorf@users.noreply.github.com>
2020-06-06 08:26:30 -0500

    Updated functions

--- R/xBoxes.R
+++ R/xBoxes.R
@@ -31,11 +31,21 @@ describeBoxes.formula <- function(formula,main=NULL,digits=3) {
 
 #### Boxplot Functions
 
-.bpvio <- function(var,loc,offset=0,scale=.6) {
+.vio <- function(var,loc,offset=0,scale=.6,sides="both") {
   y <- density(var)
   y1 <- loc+(y$y*scale)+offset
   y2 <- loc-(y$y*scale)+offset
   polygon(c(y1,rev(y2)),c(y$x,rev(y$x)),border="gray75",col="gray90")
+}
+
+.halfvio <- function(var,loc,offset=0,scale=.6) {
+  y <- density(var)
+  y1 <- loc+(y$y*scale)+offset
+  y2 <- loc-(y$y*scale)+offset
+  polygon(c(y1,seq(from=loc,to=loc,length.out=length(y2))),c(y$x,rev(y$x)),border="gray75",col="gray90")
+}
+
+.bp <- function(var,loc,offset=0,scale=.6) {
   z <- .unformatFrame(describeBoxes(var)[[1]])
   arrows(loc+offset,z[1],loc+offset,z[5],length=0,lty=2)
   rect(loc+offset-.04,z[2],loc+offset+.04,z[4],col="white",lwd=1)
@@ -43,6 +53,13 @@ describeBoxes.formula <- function(formula,main=NULL,digits=3) {
   arrows(loc+offset-.02,z[1],loc+offset+.02,z[1],length=0,lwd=1)
   arrows(loc+offset-.02,z[5],loc+offset+.02,z[5],length=0,lwd=1)
 }
+
+.dots <- function(var,loc,offset=0,jitter=0,col="gray60",pch=1) {
+  newx <- NULL
+  for(i in 1:length(var)) {newx[i] <- loc+runif(1,min=0,max=jitter)+offset}
+  points(newx,var,col=col,pch=pch)
+}
+
 
 plotBoxes <- function(...) 
   UseMethod("plotBoxes")
@@ -59,7 +76,8 @@ plotBoxes.default <- function(...,main=NULL,ylab="Outcome",xlab="",ylim=NULL) {
   nvars <- length(data)
   plot(NULL,bty="l",xaxt="n",main=main,xlab=xlab,ylab=ylab,xlim=c(.5,nvars+.5),ylim=ylim,cex.lab=1.3)
   axis(1,1:nvars,vars)
-  for(i in 1:nvars) {.bpvio(data[,i],i)}
+  for(i in 1:nvars) {.halfvio(data[,i],i)}
+  for(i in 1:nvars) {.bp(data[,i],i)}  
 } 
 
 plotBoxes.formula <- function(formula,main=NULL,ylab="Outcome",xlab="",ylim=NULL) {
@@ -76,5 +94,8 @@ plotBoxes.formula <- function(formula,main=NULL,ylab="Outcome",xlab="",ylim=NULL
   ngroups <- nlevels(group)
   plot(NULL,bty="l",xaxt="n",main=main,xlab=xlab,ylab=ylab,xlim=c(.5,ngroups+.5),ylim=ylim,cex.lab=1.3)
   axis(1,1:ngroups,groups)
-  for(i in 1:ngroups) {with(subset(data,group==groups[i]),.bpvio(outcome,i))}
+  for(i in 1:ngroups) {with(subset(data,group==groups[i]),.vio(outcome,i))}
+  for(i in 1:ngroups) {with(subset(data,group==groups[i]),.bp(outcome,i))}
 }
+
+
--- R/xData.R
+++ R/xData.R
@@ -8,7 +8,7 @@
 plotData <- function(x,...)
   UseMethod("plotData")
 
-plotData.default <- function(...,add=FALSE,main=NULL,ylab="Outcome",xlab="",method="jitter",col="gray60",pch=16) {
+plotData.default <- function(...,add=FALSE,main=NULL,ylab="Outcome",xlab="",method="jitter",col="gray60",pch=1) {
   data <- data.frame(...)
   ylimrange <- range(pretty(c(floor(min(data-.4)),ceiling(max(data)+.4))))
   xlimrange <- c(.5,ncol(data)+.5)
@@ -19,7 +19,7 @@ plotData.default <- function(...,add=FALSE,main=NULL,ylab="Outcome",xlab="",meth
   stripchart(data,add=add,xlim=xlimrange,ylim=ylimrange,at=mn:mx,vertical=TRUE,method=method,main=main,ylab=ylab,xlab=xlab,jitter=0.08,col=col,pch=pch,cex.lab=1.3)
 } 
 
-plotData.formula <- function(formula,add=FALSE,main=NULL,ylab=NULL,xlab="",method="jitter",col="gray60",pch=16,...) {
+plotData.formula <- function(formula,add=FALSE,main=NULL,ylab=NULL,xlab="",method="jitter",col="gray60",pch=1,...) {
   x <- eval(formula[[3]])
   adjustX <- as.numeric(x)+.15
   mn <- min(adjustX,na.rm=TRUE)
@@ -38,14 +38,14 @@ plotData.formula <- function(formula,add=FALSE,main=NULL,ylab=NULL,xlab="",metho
 addplotData <- function(x,...) 
   UseMethod("addplotData")
 
-addplotData.default <- function(...,method="jitter",col="gray60",pch=16) {
+addplotData.default <- function(...,method="jitter",col="gray60",pch=1) {
   data <- data.frame(...)
   mx <- ncol(data)+.15
   mn <- 1+.15
   stripchart(data,add=TRUE,at=mn:mx,vertical=TRUE,method=method,jitter=0.08,pch=pch,col=col)
 }
 
-addplotData.formula <- function(formula,method="jitter",col="gray60",pch=16,...) {
+addplotData.formula <- function(formula,method="jitter",col="gray60",pch=1,...) {
   x <- eval(formula[[3]])
   adjustX <- as.numeric(x)+.15
   mn <- min(adjustX,na.rm=TRUE)
