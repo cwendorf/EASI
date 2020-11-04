73dcee8f38e8336975f24c05fcc654eabab51b5f
cwendorf <45096238+cwendorf@users.noreply.github.com>
2020-11-04 04:31:31 -0600

    Update xCorrelationComparison.R

--- R/xCorrelationComparison.R
+++ R/xCorrelationComparison.R
@@ -6,20 +6,26 @@
 estimateCorrelationsComparison <- estimateCorrelationComparison <- function(x,...) 
   UseMethod("estimateCorrelationComparison")
 
-estimateCorrelationComparison.default <- function(CorrEst1,CorrEst2,main=NULL,digits=3) {
+estimateCorrelationComparison.default <- function(CorrEst1,CorrEst2,main=NULL,labels=NULL,digits=3) {
+  Est1 <- .unformatFrame(CorrEst1[[1]])
+  Est2 <- .unformatFrame(CorrEst2[[1]])
+  results <- rbind(Est1,Est2)
+  if(is.null(labels)) {rownames(results) <- c("CorrEst1","CorrEst2")} else {rownames(results) <- labels} 
+  results <- .formatList(list(results),digits=digits)
+  names(results) <- "Confidence Intervals for the Correlations"     
   Diff <- estimateCorrelationDifference(CorrEst1,CorrEst2)
-  results <- c(CorrEst1,CorrEst2,Diff)
+  results <- c(results,Diff)
   return(results)
-}  
+}   
 
 ### Confidence Interval Plot Functions
 
 plotCorrelationsComparison <- plotCorrelationComparison <- function(x,...) 
   UseMethod("plotCorrelationComparison")
 
-plotCorrelationComparison.default <- function(CorrEst1,CorrEst2,main=NULL,ylab="Outcome",xlab="",rope=NULL,labels=NULL,values=TRUE,ylim=NULL,digits=3) {
-  results <- estimateCorrelationComparison(CorrEst1,CorrEst2,main=main,digits=digits)
-  results <- rbind(.unformatFrame(results[[1]][,c(1,3,4)]),.unformatFrame(results[[2]][,c(1,3,4)]),.unformatFrame(results[[3]][,c(1,2,3)]))
+plotCorrelationComparison.default <- function(CorrEst1,CorrEst2,main=NULL,ylab="Correlation",xlab="",rope=NULL,labels=NULL,values=TRUE,ylim=c(-1,1),digits=3) {
+  results <- estimateCorrelationComparison(CorrEst1,CorrEst2,main=main,labels=labels,digits=digits)
+  results <- rbind(.unformatFrame(results[[1]][,c(1,3,4)]),.unformatFrame(results[[2]][,c(1,2,3)]))
   if(is.null(main)) {main="Confidence Intervals for the Correlation Comparison"}
   .cipComp(results,main=main,ylab=ylab,xlab=xlab,rope=rope,values=values,ylim=ylim,digits=digits,connect=TRUE)
 }
