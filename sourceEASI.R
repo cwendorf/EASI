c77a29bf29436f4fbbb802e51e3ba780a6bfabc8
cwendorf <45096238+cwendorf@users.noreply.github.com>
2020-11-14 04:55:26 -0600

    Update sourceEASI.R

--- sourceEASI.R
+++ sourceEASI.R
@@ -9,6 +9,8 @@ source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xBoxes.R")
 source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xViolins.R")
 
 source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xCorrelations.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xCorrelationDifference.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xCorrelationComparison.R")
 
 source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xMeans.R")
 source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xStandardizedMeans.R")
@@ -22,6 +24,7 @@ source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xMeanSubsets.R")
 source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xStandardizedMeanContrast.R")
 
 source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xMeansOmnibus.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xMeansEffect.R")
 source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xMeansPairwise.R")
 source("http://raw.githubusercontent.com/cwendorf/EASI/master/R/xStandardizedMeansPairwise.R")
 
