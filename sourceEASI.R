b059ff358dfb4e484fb52a1fe84ee0fe3b0b8705
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-03-16 11:38:51 -0500

    Update sourceEASI.R

--- sourceEASI.R
+++ sourceEASI.R
@@ -3,18 +3,20 @@
 
 source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/cipX.R")
 source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/formatX.R")
-source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xDistributions.R")
 
 source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xData.R")
-source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xBoxes.R")
-source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xViolins.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xDensity.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xDistribution.R")
 source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xCateyes.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xBoxes.R")
 
 source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xCorrelations.R")
 source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xCorrelationDifference.R")
 source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xCorrelationComparison.R")
 
 source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xRegression.R")
+source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xStandardizedRegression.R")
+
 source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xRegressionOmnibus.R")
 source("http://raw.githubusercontent.com/cwendorf/EASI/main/R/xRegressionEffect.R")
 
