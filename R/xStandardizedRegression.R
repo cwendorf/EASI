2c258831a1bd86f84f1522a348a42e253696c35d
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-04-27 07:26:09 -0500

    Update xStandardizedRegression.R

--- R/xStandardizedRegression.R
+++ R/xStandardizedRegression.R
@@ -18,11 +18,13 @@ estimateStandardizedRegression.wss <- function(PredStats,CritStats,CorrStats,con
 }
 
 estimateStandardizedRegression.default <- function(Predictors,Criterion,conf.level=.95,main=NULL,digits=3) {
-  PredStats <- .unformatFrame(describeMeans(Predictors)[[1]])
+  Pred <- cbind(Predictors)
+  if(is.null(ncol(Predictors))) {colnames(Pred) <- deparse(substitute(Predictors))}
+  PredStats <- .unformatFrame(describeMeans(Pred)[[1]])
   class(PredStats) <- "wss"
   CritStats <- .unformatFrame(describeMeans(Criterion)[[1]])
   class(CritStats) <- "wss"
-  CorrStats <- .unformatFrame(describeCorrelations(Predictors,Criterion)[[1]])
+  CorrStats <- .unformatFrame(describeCorrelations(Pred,Criterion)[[1]])
   class(CorrStats) <- "wss"
   estimateStandardizedRegression(PredStats,CritStats,CorrStats,conf.level=conf.level,main=main,digits=digits)
 }
@@ -39,7 +41,9 @@ plotStandardizedRegression.wss <- function(PredStats,CritStats,CorrStats,main=NU
 }
 
 plotStandardizedRegression.default <- function(Predictors,Criterion,main=NULL,ylab="Standardized Coefficient",xlab="",mu=NULL,rope=NULL,conf.level=.95,values=TRUE,ylim=NULL,digits=3) {
-  results <- .unformatFrame(estimateStandardizedRegression(Predictors,Criterion,conf.level=conf.level,main=main,digits=digits)[[1]][,c(1,3,4)])
+  Pred <- cbind(Predictors)
+  if(is.null(ncol(Predictors))) {colnames(Pred) <- deparse(substitute(Predictors))}
+  results <- .unformatFrame(estimateStandardizedRegression(Pred,Criterion,conf.level=conf.level,main=main,digits=digits)[[1]][,c(1,3,4)])
   if(is.null(main)) {if(nrow(results)>1) {main="Confidence Intervals for the \n Standardized Regression Coefficients"} else {main="Confidence Interval for the \n Standardized Regression Coefficient"}}
  .cipMain(results,main=main,ylab=ylab,xlab=xlab,mu=mu,rope=rope,values=values,ylim=ylim,digits=digits,connect=FALSE,pch=22)
 }
