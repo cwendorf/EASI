03ee7483afb18070c74eba6c4783b16fe2369374
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-10-20 19:00:46 -0500

    Update xMeans.R

--- R/xMeans.R
+++ R/xMeans.R
@@ -1,6 +1,46 @@
 # Estimation Approach to Statistical Inference
 ## Means
 
+## Basic Functions
+
+moment <- function(x, order = 1, center = FALSE, absolute = FALSE, na.rm = FALSE) {
+  if (na.rm) x <- x[!is.na(x)]
+  if (center) x <- x - mean(x)
+  if (absolute) x <- abs(x)
+  sum(x ^ order) / length(x)
+}
+
+skewness <- function(x, na.rm = FALSE, type = 2) {
+  if(any(ina <- is.na(x))) {
+    if(na.rm) x <- x[!ina]
+    else return(NA)}
+  if(!(type %in% (1 : 3))) stop("Invalid 'type' argument.")
+  n <- length(x)
+  x <- x - mean(x)
+  y <- sqrt(n) * sum(x ^ 3) / (sum(x ^ 2) ^ (3/2))
+  if(type == 2) {
+    if(n < 3) stop("Need at least 3 complete observations.")
+    y <- y * sqrt(n * (n - 1)) / (n - 2)} 
+    else if(type == 3) y <- y * ((1 - 1 / n)) ^ (3/2)
+  y
+}
+
+kurtosis <- function(x, na.rm = FALSE, type = 2) {
+  if(any(ina <- is.na(x))) {
+    if(na.rm) x <- x[!ina]
+    else return(NA)}
+  if(!(type %in% (1 : 3))) stop("Invalid 'type' argument.")
+  n <- length(x)
+  x <- x - mean(x)
+  r <- n * sum(x ^ 4) / (sum(x ^ 2) ^ 2)
+  y <- if(type == 1) r - 3
+  else if(type == 2) {
+    if(n < 4) stop("Need at least 4 complete observations.")
+    ((n + 1) * (r - 3) + 6) * (n - 1) / ((n - 2) * (n - 3))}
+    else r * (1 - 1 / n) ^ 2 - 3
+  y
+}
+
 ### Descriptives
 
 describeMeans <- function(x,...) 
@@ -11,7 +51,9 @@ describeMeans.default <- function(...,main=NULL,digits=3) {
   N <- sapply(data,length)
   M <- sapply(data,mean,na.rm=TRUE)
   SD <- sapply(data,sd,na.rm=TRUE)
-  results <- cbind(N=N,M=M,SD=SD)
+  Skew <- sapply(data,skewness,na.rm=TRUE)
+  Kurt <- sapply(data,kurtosis,na.rm=TRUE)
+  results <- cbind(N=N,M=M,SD=SD,Skew=Skew,Kurt=Kurt)
   results <- .formatList(list(results),digits=digits)  
   if(is.null(main)) {names(results) <- "Descriptive Statistics for the Data"} else {names(results) <- main}  
   return(results)
