25b22f4d923daa7d8e8c573ece28100023932d39
Craig Wendorf <45096238+cwendorf@users.noreply.github.com>
2021-10-22 14:06:27 -0500

    Update xMeansPairwise.R

--- R/xMeansPairwise.R
+++ R/xMeansPairwise.R
@@ -6,7 +6,7 @@
 estimateMeansPairwise <- function(x,...) 
   UseMethod("estimateMeansPairwise")
 
-estimateMeansPairwise.wss <- function(SumStats,CorrStats,conf.level=.95,mu=0,main=NULL,digits=3,...){
+estimateMeansPairwise.wss <- function(SumStats,CorrStats,conf.level=.95,mu=0,posthoc="LSD",main=NULL,digits=3,...){
   N <- SumStats[,"N"]
   M <- SumStats[,"M"]
   SD <- SumStats[,"SD"]
@@ -23,7 +23,8 @@ estimateMeansPairwise.wss <- function(SumStats,CorrStats,conf.level=.95,mu=0,mai
     MD <- M[rn[j]]-M[rn[i]]-mu
     SEd <- sqrt(SE[rn[i]]^2+SE[rn[j]]^2-2*CorrStats[rn[i],rn[j]]*SE[rn[i]]*SE[rn[j]])
     df <- min(N)-1
-    tcrit <- qt((1-conf.level)/2,df,lower.tail=FALSE)
+    if(posthoc="LSD") (tcrit <- qt((1-conf.level)/2,df,lower.tail=FALSE))
+    if(posthoc="HSD") (tcrit <- qtukey(conf.level,2,df=df)/sqrt(2))
     LL <- MD-tcrit*SEd
     UL <- MD+tcrit*SEd
     results[comp,] <- c(MD,SEd,df,LL,UL)
@@ -34,7 +35,7 @@ estimateMeansPairwise.wss <- function(SumStats,CorrStats,conf.level=.95,mu=0,mai
   return(results)
 }
 
-estimateMeansPairwise.bss <- function(SumStats,conf.level=.95,mu=0,main=NULL,digits=3,...){
+estimateMeansPairwise.bss <- function(SumStats,conf.level=.95,mu=0,posthoc="LSD",main=NULL,digits=3,...){
   N <- SumStats[,"N"]
   M <- SumStats[,"M"]
   SD <- SumStats[,"SD"]
@@ -51,7 +52,8 @@ estimateMeansPairwise.bss <- function(SumStats,conf.level=.95,mu=0,main=NULL,dig
     MD <- M[rn[j]]-M[rn[i]]-mu
     SEd <- sqrt( (SD[rn[i]]^2/N[rn[i]]) + (SD[rn[j]]^2/N[rn[j]]) )
     df <- ((SD[rn[i]]^2/N[rn[i]] + SD[rn[j]]^2/N[rn[j]])^2 )/( (SD[rn[i]]^2/N[rn[i]])^2/(N[rn[i]]-1) + (SD[rn[j]]^2/N[rn[j]])^2/(N[rn[j]]-1) )
-    tcrit <- qt((1-conf.level)/2,df,lower.tail=FALSE)
+    if(posthoc="LSD") (tcrit <- qt((1-conf.level)/2,df,lower.tail=FALSE))
+    if(posthoc="HSD") (tcrit <- qtukey(conf.level,2,df)/sqrt(2))
     LL <- MD-tcrit*SEd
     UL <- MD+tcrit*SEd
     results[comp,] <- c(MD,SEd,df,LL,UL)
@@ -62,17 +64,17 @@ estimateMeansPairwise.bss <- function(SumStats,conf.level=.95,mu=0,main=NULL,dig
   return(results)
 }
 
-estimateMeansPairwise.default <- function(...,conf.level=.95,mu=0,main=NULL,digits=3){
+estimateMeansPairwise.default <- function(...,conf.level=.95,mu=0,posthoc="LSD",main=NULL,digits=3){
   SumStats <- .describeMeans(...)
   class(SumStats) <- "wss"
   CorrStats <- .describeCorrelations(...)
-  estimateMeansPairwise(SumStats,CorrStats,conf.level=conf.level,main=main,digits=digits)
+  estimateMeansPairwise(SumStats,CorrStats,conf.level=conf.level,posthoc=posthoc,main=main,digits=digits)
 }
 
-estimateMeansPairwise.formula <- function(formula,conf.level=.95,mu=0,main=NULL,digits=3,...){
+estimateMeansPairwise.formula <- function(formula,conf.level=.95,mu=0,posthoc="LSD",main=NULL,digits=3,...){
   SumStats <- .describeMeans(formula)
   class(SumStats) <- "bss"
-  estimateMeansPairwise(SumStats,conf.level=conf.level,main=main,digits=digits)
+  estimateMeansPairwise(SumStats,conf.level=conf.level,posthoc=posthoc,main=main,digits=digits)
 }
 
 ### Null Hypothesis Significance Tests 
@@ -80,7 +82,7 @@ estimateMeansPairwise.formula <- function(formula,conf.level=.95,mu=0,main=NULL,
 testMeansPairwise <- function(x,...) 
   UseMethod("testMeansPairwise")
 
-testMeansPairwise.wss <- function(SumStats,CorrStats,mu=0,main=NULL,digits=3,...){
+testMeansPairwise.wss <- function(SumStats,CorrStats,mu=0,posthoc="LSD",main=NULL,digits=3,...){
   N <- SumStats[,"N"]
   M <- SumStats[,"M"]
   SD <- SumStats[,"SD"]
@@ -98,7 +100,8 @@ testMeansPairwise.wss <- function(SumStats,CorrStats,mu=0,main=NULL,digits=3,...
     SEd <- sqrt(SE[rn[i]]^2+SE[rn[j]]^2-2*CorrStats[rn[i],rn[j]]*SE[rn[i]]*SE[rn[j]])
     df <- min(N)-1
     t <- MD/SEd
-    p <- 2*(1 - pt(abs(t),df))
+    if(posthoc="LSD") (p <- 2*(1 - pt(abs(t),df)))
+    if(posthoc="HSD") (p <- ptukey(abs(t)*sqrt(2),2,df=df))
     results[comp,] <- c(MD,SEd,df,t,p)
    	comp <- comp+1}}
   if(is.null(main)) {if(nrow(results)>1) {main="Hypothesis Tests for the Pairwise Mean Comparisons"} else {main="Hypothesis Test for the Pairwise Mean Comparison"}}  
@@ -107,7 +110,7 @@ testMeansPairwise.wss <- function(SumStats,CorrStats,mu=0,main=NULL,digits=3,...
   return(results)
 }
 
-testMeansPairwise.bss <- function(SumStats,mu=0,main=NULL,digits=3,...){
+testMeansPairwise.bss <- function(SumStats,mu=0,posthoc="LSD",main=NULL,digits=3,...){
   N <- SumStats[,"N"]
   M <- SumStats[,"M"]
   SD <- SumStats[,"SD"]
@@ -125,7 +128,8 @@ testMeansPairwise.bss <- function(SumStats,mu=0,main=NULL,digits=3,...){
     SEd <- sqrt( (SD[rn[i]]^2/N[rn[i]]) + (SD[rn[j]]^2/N[rn[j]]) )
     df <- ((SD[rn[i]]^2/N[rn[i]] + SD[rn[j]]^2/N[rn[j]])^2 )/( (SD[rn[i]]^2/N[rn[i]])^2/(N[rn[i]]-1) + (SD[rn[j]]^2/N[rn[j]])^2/(N[rn[j]]-1) )
     t <- MD/SEd
-    p <- 2*(1 - pt(abs(t),df))
+    if(posthoc="LSD") (p <- 2*(1 - pt(abs(t),df)))
+    if(posthoc="HSD") (p <- ptukey(abs(t)*sqrt(2),2,df=df))
     results[comp,] <- c(MD,SEd,df,t,p)
    	comp <- comp+1}}
   if(is.null(main)) {if(nrow(results)>1) {main="Hypothesis Tests for the Pairwise Mean Comparisons"} else {main="Hypothesis Test for the Pairwise Mean Comparison"}}  
@@ -134,17 +138,17 @@ testMeansPairwise.bss <- function(SumStats,mu=0,main=NULL,digits=3,...){
   return(results)
 }
 
-testMeansPairwise.default <- function(...,mu=0,main=NULL,digits=3){
+testMeansPairwise.default <- function(...,mu=0,posthoc="LSD",main=NULL,digits=3){
   SumStats <- .describeMeans(...)
   class(SumStats) <- "wss"
   CorrStats <- .describeCorrelations(...)
-  testMeansPairwise(SumStats,CorrStats,mu=mu,main=main,digits=digits)
+  testMeansPairwise(SumStats,CorrStats,mu=mu,posthoc=posthoc,main=main,digits=digits)
 }
 
-testMeansPairwise.formula <- function(formula,mu=0,main=NULL,digits=3,...){
+testMeansPairwise.formula <- function(formula,mu=0,posthoc="LSD",main=NULL,digits=3,...){
   SumStats <- .describeMeans(formula)
   class(SumStats) <- "bss"
-  testMeansPairwise(SumStats,mu=mu,main=main,digits=digits)
+  testMeansPairwise(SumStats,mu=mu,posthoc=posthoc,main=main,digits=digits)
 }
 
 ### Confidence Interval Plots
@@ -152,14 +156,14 @@ testMeansPairwise.formula <- function(formula,mu=0,main=NULL,digits=3,...){
 plotMeansPairwise <- function(x,...) 
   UseMethod("plotMeansPairwise")
 
-plotMeansPairwise.default <- plotMeansPairwise.bss <- plotMeansPairwise.wss <- function(...,main=NULL,ylab="Mean Difference",xlab="",conf.level=.95,mu=0,line=NULL,rope=NULL,values=TRUE,ylim=NULL,digits=3,pch=17,col="black") {
-  results <- .unformatFrame(estimateMeansPairwise(...,conf.level=conf.level,mu=mu)[[1]][,c(1,4,5)])
+plotMeansPairwise.default <- plotMeansPairwise.bss <- plotMeansPairwise.wss <- function(...,main=NULL,ylab="Mean Difference",xlab="",conf.level=.95,mu=0,posthoc="LSD",line=NULL,rope=NULL,values=TRUE,ylim=NULL,digits=3,pch=17,col="black") {
+  results <- .unformatFrame(estimateMeansPairwise(...,conf.level=conf.level,mu=mu,posthoc=posthoc)[[1]][,c(1,4,5)])
   if(is.null(main)) {if(nrow(results)>1) {main="Confidence Intervals for the \n Pairwise Mean Comparisons"} else {main="Confidence Interval for the \n Pairwise Mean Comparison"}}  
   .cipMain(results,main=main,ylab=ylab,xlab=xlab,line=line,rope=rope,values=values,ylim=ylim,digits=digits,connect=FALSE,pch=pch,col=col) 
 }
 
-plotMeansPairwise.formula <- function(formula,main=NULL,ylab="Mean Difference",xlab="",conf.level=.95,mu=0,line=NULL,rope=NULL,values=TRUE,ylim=NULL,digits=3,pch=17,col="black") {
-  results <- .unformatFrame(estimateMeansPairwise(formula,conf.level=conf.level,mu=mu)[[1]][,c(1,4,5)])
+plotMeansPairwise.formula <- function(formula,main=NULL,ylab="Mean Difference",xlab="",conf.level=.95,mu=0,posthoc="LSD",line=NULL,rope=NULL,values=TRUE,ylim=NULL,digits=3,pch=17,col="black") {
+  results <- .unformatFrame(estimateMeansPairwise(formula,conf.level=conf.level,mu=mu,posthoc=posthoc)[[1]][,c(1,4,5)])
   if(is.null(main)) {if(nrow(results)>1) {main="Confidence Intervals for the \n Pairwise Mean Comparisons"} else {main="Confidence Interval for the \n Pairwise Mean Comparison"}}  
   .cipMain(results,main=main,ylab=ylab,xlab=xlab,line=line,rope=rope,values=values,ylim=ylim,digits=digits,connect=FALSE,pch=pch,col=col) 
 }
