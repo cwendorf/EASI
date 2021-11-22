0745f61debfd64648e349429320929174a04e674
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-11-22 15:58:35 -0600

    Update xMeansOmnibusMultifactor.R

--- R/xMeansOmnibusMultifactor.R
+++ R/xMeansOmnibusMultifactor.R
@@ -9,19 +9,23 @@
 .describeMeansOmnibusMultifactor.default <- function(...,by) {
   data <- data.frame(...)
   columns <- dim(data)[2]
-  data <- data.frame(...,Factor=by)
+  data <- data.frame(...,by)
+  byname <- deparse(substitute(by))
+  intxname <- paste("Measures",byname,sep=":")
   dataLong <- reshape(data,varying=1:columns,v.names="Outcome",timevar="Measures",idvar="Subjects",direction="long")
   dataLong$Subjects <- as.factor(dataLong$Subjects)
   dataLong$Measures <- as.factor(dataLong$Measures)
-  dataLong$Factor <- as.factor(dataLong$Factor)
-  object <- aov(Outcome~Factor+(Measures*Factor)+Error(Subjects/Measures),data=dataLong)
+  dataLong$by <- as.factor(dataLong$by)
+  object <- aov(Outcome~by+(Measures*by)+Error(Subjects/Measures),data=dataLong)
   w <- summary(object)
   x <- w[[1]][[1]]
   results1 <- cbind(x[2],x[1],x[3])
   colnames(results1) <- c("SS","df","MS")
+  rownames(results1) <- c(byname,"Residuals")
   y <- w[[2]][[1]]
   results2 <- cbind(y[2],y[1],y[3])
   colnames(results2) <- c("SS","df","MS")
+  rownames(results2) <- c("Measures",intxname,"Residuals")
   results <- list(results1,results2)
   names(results) <- c("Between Subjects","Within Subjects")
   return(results)
@@ -29,12 +33,15 @@
 
 .describeMeansOmnibusMultifactor.formula <- function(formula,by,...) {
   Group <- eval(formula[[3]])
+  groupname <- deparse(formula[[3]])
   Outcome <- eval(formula[[2]])
-  Factor <- by
-  object <- aov(Outcome~Group+Factor+(Group*Factor))
+  byname <- deparse(substitute(by))
+  intxname <- paste(groupname,byname,sep=":")
+  object <- aov(Outcome~by+Group+(by*Group))
   x <- summary(object)[[1]]
   results <- cbind(x[2],x[1],x[3])
   colnames(results) <- c("SS","df","MS")
+  rownames(results) <- c(groupname,byname,intxname,"Residuals")
   results <- list(results)
   names(results) <- "Between Subjects"
   return(results)
@@ -57,21 +64,25 @@ describeMeansOmnibusMultifactor <- function(...,main=NULL,digits=3) {
 .testMeansOmnibusMultifactor.default <- function(...,by) {
   data <- data.frame(...)
   columns <- dim(data)[2]
-  data <- data.frame(...,Factor=by)
+  data <- data.frame(...,by)
+  byname <- deparse(substitute(by))
+  intxname <- paste("Measures",byname,sep=":")
   dataLong <- reshape(data,varying=1:columns,v.names="Outcome",timevar="Measures",idvar="Subjects",direction="long")
   dataLong$Subjects <- as.factor(dataLong$Subjects)
   dataLong$Measures <- as.factor(dataLong$Measures)
-  dataLong$Factor <- as.factor(dataLong$Factor)
-  object <- aov(Outcome~Factor+(Measures*Factor)+Error(Subjects/Measures),data=dataLong)
+  dataLong$by <- as.factor(dataLong$by)
+  object <- aov(Outcome~by+(Measures*by)+Error(Subjects/Measures),data=dataLong)
   w <- summary(object)
   x <- w[[1]][[1]]
   results1 <- cbind(x[4],x[5])
   results1 <- results1[1,]
   colnames(results1) <- c("F","p")
+  rownames(results1) <- byname  
   y <- w[[2]][[1]]
   results2 <- cbind(y[4],y[5])
   results2 <- results2[1:2,]
   colnames(results2) <- c("F","p")
+  rownames(results2) <- c("Measures",intxname)  
   results <- list(results1,results2)
   names(results) <- c("Between Subjects","Within Subjects")
   return(results)
@@ -79,13 +90,16 @@ describeMeansOmnibusMultifactor <- function(...,main=NULL,digits=3) {
 
 .testMeansOmnibusMultifactor.formula <- function(formula,by,...) {
   Group <- eval(formula[[3]])
+  groupname <- deparse(formula[[3]])
   Outcome <- eval(formula[[2]])
-  Factor <- by
-  object <- aov(Outcome~Group+Factor+(Group*Factor))
+  byname <- deparse(substitute(by))
+  intxname <- paste(groupname,byname,sep=":")
+  object <- aov(Outcome~by+Group+(by*Group))
   x <- summary(object)[[1]]
   results <- cbind(x[4],x[5])
   results <- results[1:3,]
   colnames(results) <- c("F","p")
+  rownames(results) <- c(groupname,byname,intxname)  
   results <- list(results)
   names(results) <- "Between Subjects"
   return(results)
