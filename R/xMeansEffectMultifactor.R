812958fea67c780d4cdb3c868f4b11a391f64b8b
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-11-29 05:06:07 -0600

    Updated multifactor functions

--- R/xMeansEffectMultifactor.R
+++ R/xMeansEffectMultifactor.R
@@ -68,7 +68,7 @@ estimateMeansEffectMultifactor <- function(...,main=NULL,digits=3) {
   results <- .estimateMeansEffectMultifactor(...)
   nm = names(results)
   results <- .formatList(results,digits=digits)
-  if(is.null(main)) {main <- "Proportion of Variance Accounted"} 
+  if(is.null(main)) {main <- "Proportion of Variance Accounted For"} 
   names(results) = paste(main,nm,sep=": ")
   return(results)
 }
