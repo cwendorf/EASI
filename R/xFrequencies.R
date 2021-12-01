5c91607d04eff612b86e69aa0f983aba7ea3aa27
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-11-30 20:39:56 -0600

    Refactored frequencies function

--- R/xFrequencies.R
+++ R/xFrequencies.R
@@ -3,31 +3,36 @@
 
 ### Descriptives
 
-.describeFrequencies <- function(x,main=NULL,digits=3) {
-  Freq=table(x)
-  Perc=prop.table(table(x))
-  CumFreq=cumsum(table(x))
-  CumPerc=cumsum(table(x))/summary(table(x))[[2]]
+.descFreq <- function(x) {
+  Freq <- table(x)
+  Perc <- prop.table(table(x))
+  CumFreq <- cumsum(table(x))
+  CumPerc <- cumsum(table(x))/summary(table(x))[[2]]
   results <- cbind(Freq,Perc,CumFreq,CumPerc)
-  results <- .formatList(list(results),digits=digits)
-  if(is.null(main)) {names(results) <- "Frequency Distribution for the Variable"} else {names(results) <- main}  
   return(results)  
 }
 
-describeFrequencies <- function(x,...) 
-  UseMethod("describeFrequencies")
+.describeFrequencies <- function(x,...) 
+  UseMethod(".describeFrequencies")
 
-describeFrequencies.default <- function(...,main=NULL,digits=3) {
+.describeFrequencies.default <- function(...) {
   data <- data.frame(...)
-  results <- lapply(data,FUN=.describeFrequencies,main=main,digits=digits)
-  results <- unlist(results,recursive=FALSE)
+  results <- lapply(data,FUN=.descFreq)
   return(results)
 }
 
-describeFrequencies.formula <- function(formula,main=NULL,digits=3) {
+.describeFrequencies.formula <- function(formula) {
   group <- eval(formula[[3]])
   outcome <- eval(formula[[2]])
-  results <- tapply(outcome,group,FUN=.describeFrequencies,simplify=FALSE)
-  results <- unlist(results,recursive=FALSE)  
+  results <- tapply(outcome,group,FUN=.descFreq)
+  return(results)
+}
+
+describeFrequencies <- function(...,main=NULL,digits=3) {
+  results <- .describeFrequencies(...) 
+  nm = names(results)
+  results <- .formatList(results,digits=digits)
+  if(is.null(main)) {main <- "Frequency Distribution"} 
+  names(results) = paste(main,nm,sep=": ")
   return(results)
 }
