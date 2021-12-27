a9baeb704476e704f2610b0918addd1572ea0e75
cwendorf <45096238+cwendorf@users.noreply.github.com>
2021-12-27 15:52:06 -0600

    Update xDiamonds.R

--- R/xDiamonds.R
+++ R/xDiamonds.R
@@ -16,20 +16,31 @@
 plotDiamonds <- function(x,...) 
   UseMethod("plotDiamonds")
 
+plotDiamonds.default <- function(...,main=NULL,digits=3,ylab="Outcome",xlab="",mu=0,line=NULL,rope=NULL,conf.level=.95,ylim=NULL,add=FALSE,pch=16,col="black",hw=.2) {
+  results <- estimateMeans(...,mu=mu,conf.level=conf.level,main=main,digits=digits)
+  if(is.null(main)) {main=names(results)} 
+  results <- .unformatFrame(.deList(results))[,c("Est","LL","UL")]
+  if(!add) .plotMain(results,main=main,ylab=ylab,xlab=xlab,ylim=ylim)
+  if(!is.null(line)) {abline(h=line,lty=2,col="black")}
+  if(!is.null(rope)) {rect(0,rope[1],nrow(results)+1,rope[2],col=.colorTransparent("black",15),border=NA)} 
+  z <- asplit(results,1)
+  invisible(mapply(.diamond,z,loc=1:nrow(results),col=col,hw=hw))
+}
+
 plotDiamonds.list <- function(results,mu=0,conf.level=.95,add=FALSE,main=NULL,ylab="Outcome",xlab="",slab="Difference",ylim=NULL,offset=0,col="black",hw=.2) {
   if(length(results)==1) {
     if(is.null(main)) main=names(results[1])
-    results <- .unformatFrame(.deList(results)[,c(1,4,5)])
+    results <- .unformatFrame(.deList(results))[,c("Est","LL","UL")]
     if(!add) .plotMain(results,main=main,ylab=ylab,xlab=xlab,ylim=ylim)
     z <- asplit(results,1)
     invisible(mapply(.diamond,z,loc=1:nrow(results),col=col,hw=hw))
   }
   if(length(results)==2) {
     if(is.null(main)) main=names(results[2])
-    graph <- rbind(.unformatFrame(results[[1]][,c(1,4,5)]),.unformatFrame(results[[2]][,c(1,4,5)]))
+    graph <- .unformatFrame(.deList(results))[,c("Est","LL","UL")]
     graph[3,1] <- graph[3,1]+graph[1,1]
     if(!add) .plotComp(graph,main=main,ylab=ylab,xlab=xlab,ylim=ylim,slab=slab)
-    results <- rbind(.unformatFrame(results[[1]][,c(1,4,5)]),.unformatFrame(results[[2]][,c(1,4,5)]))
+    results <- .unformatFrame(.deList(results))[,c("Est","LL","UL")]    
     results[3,] <- results[3,]+results[1,1]
     z <- asplit(results,1)
     invisible(mapply(.diamond,z,loc=1:nrow(results),col=col,hw=hw))
