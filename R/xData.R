aa9661a2fc5b734ab9fdddd0667b5c1d8d290543
cwendorf <45096238+cwendorf@users.noreply.github.com>
2022-01-08 15:49:01 -0600

    Updated offset and connections

--- R/xData.R
+++ R/xData.R
@@ -12,27 +12,26 @@
 plotData <- function(x,...)
   UseMethod("plotData")
 
-plotData.default <- function(...,add=FALSE,main=NULL,ylab="Outcome",xlab="",offset=.12,method="stack",jitter=.05,col="black",pch=16) {
+plotData.default <- function(...,add=FALSE,main=NULL,ylab="Outcome",xlab="",offset=.12,method="stack",jitter=.05,col="black",pch=16,lty="solid",connect=TRUE) {
   data <- data.frame(...)
   ylimrange <- range(pretty(c(floor(min(data)-.5),ceiling(max(data)+.5))))
   xlimrange <- c(.5,ncol(data)+.5)
-  mx <- ncol(data)+offset
-  mn <- 1+offset
+  loc <- (1:ncol(data))+offset
+  lesscol <- ncol(data)-1
   par(bty="l",xaxs="i",yaxs="i")
   if(is.null(main)) {main="Data for the Variables"}
-  stripchart(data,add=add,xlim=xlimrange,ylim=ylimrange,at=mn:mx,vertical=TRUE,method=method,main=main,ylab=ylab,xlab=xlab,jitter=jitter,col=.colorTransparent(col,70),pch=pch,cex.lab=1.15)
+  stripchart(data,add=add,xlim=xlimrange,ylim=ylimrange,at=loc,vertical=TRUE,method=method,main=main,ylab=ylab,xlab=xlab,jitter=jitter,col=.colorTransparent(col,70),pch=pch,cex.lab=1.15)
+  if(connect && method!="jitter") {for(i in 1:lesscol) {segments(loc[i],data[,i],loc[i+1],data[,i+1],col=.colorTransparent(col,70),lty=lty)}}
 } 
 
 plotData.formula <- function(formula,add=FALSE,main=NULL,ylab=NULL,xlab="",offset=.12,method="stack",jitter=.05,col="black",pch=16,...) {
   x <- eval(formula[[3]])
-  adjustX <- as.numeric(x)+offset
-  mn <- min(adjustX,na.rm=TRUE)
-  mx <- max(adjustX,na.rm=TRUE)
+  loc <- (1:nlevels(x))+offset
   y <- eval(formula[[2]])
   ylimrange <- range(pretty(c(floor(min(y)-.5),ceiling(max(y)+.5))))
   xlimrange <- c(.5,nlevels(x)+.5)
   par(bty="l",xaxs="i",yaxs="i")
   if(is.null(main)) {main="Data for the Groups"}
   if(is.null(ylab)) {ylab=all.vars(formula)[1]}  
-  stripchart(formula,add=add,xlim=xlimrange,ylim=ylimrange,at=mn:mx,vertical=TRUE,method=method,main=main,ylab=ylab,xlab=xlab,jitter=jitter,col=.colorTransparent(col,70),pch=pch,cex.lab=1.15,...)
+  stripchart(formula,add=add,xlim=xlimrange,ylim=ylimrange,at=loc,vertical=TRUE,method=method,main=main,ylab=ylab,xlab=xlab,jitter=jitter,col=.colorTransparent(col,70),pch=pch,cex.lab=1.15,...)
 }
