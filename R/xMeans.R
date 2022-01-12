2e59e46dadf96beb20471e1bc28807c856a3ec15
cwendorf <45096238+cwendorf@users.noreply.github.com>
2022-01-11 18:54:47 -0600

    Update xMeans.R

--- R/xMeans.R
+++ R/xMeans.R
@@ -139,10 +139,10 @@ testMeans <- function(...,main=NULL,digits=3) {
 
 ### Confidence Interval Plots
 
-plotMeans <- function(...,main=NULL,digits=3,ylab="Outcome",xlab="",mu=0,line=NULL,rope=NULL,conf.level=.95,values=TRUE,ylim=NULL,add=FALSE,pch=16,col="black") {
+plotMeans <- function(...,main=NULL,digits=3,ylab="Outcome",xlab="",mu=0,line=NULL,rope=NULL,conf.level=.95,values=TRUE,ylim=NULL,add=FALSE,pch=16,pos=2,col="black") {
   results <- estimateMeans(...,mu=mu,conf.level=conf.level,main=main,digits=digits)
   if (length(list(...))>1) {connect=TRUE} else if (class(...)=="wss") {connect=TRUE} else {connect=FALSE}
   if(is.null(main)) {main=names(results)} 
   results <- .unformatFrame(results[[1]][,c(1,4,5)])
- .intervalsMain(results,main=main,ylab=ylab,xlab=xlab,line=line,rope=rope,values=values,ylim=ylim,digits=digits,connect=connect,add=add,pch=pch,col=col)
+ .intervalsMain(results,main=main,ylab=ylab,xlab=xlab,line=line,rope=rope,values=values,ylim=ylim,digits=digits,connect=connect,add=add,pch=pch,col=col,pos=pos)
 }
