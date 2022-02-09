3a760bf4432497610b0663d355118e30839d41b1
cwendorf <45096238+cwendorf@users.noreply.github.com>
2022-02-09 16:11:50 -0600

    Update xMeansPairwise.R

--- R/xMeansPairwise.R
+++ R/xMeansPairwise.R
@@ -156,3 +156,43 @@ plotMeansPairwise <- function(...,main=NULL,digits=3,ylab="Mean Difference",xlab
   plotIntervals(results,add=add,main=main,xlab=xlab,ylab=ylab,ylim=ylim,values=values,line=line,rope=rope,digits=digits,connect=connect,pos=pos,col=col,offset=offset,intervals=intervals)
   invisible(eval(...))
 }
+
+plotMeansDiffogram <- function(...,main=NULL,ylab="",xlab="",conf.level=.95,ylim=NULL,pch=17,col=NULL) {
+  dm <- .describeMeans(...)
+  emp <- .estimateMeansPairwise(...,conf.level=conf.level)
+  dif <- (emp[,5]-emp[,4])/4
+  M <- dm[,"M"]
+  rn <- rownames(dm)
+  nr <- nrow(dm)
+  ncomp <- (nr)*(nr-1)/2
+  fm <- data.frame(matrix(ncol=2,nrow=ncomp))
+  comp <- 1
+  for( i in 1:(nr-1) ){
+  for( j in (i+1):nr ){
+    M1 <- M[rn[i]]
+    M2 <- M[rn[j]]
+    fm[comp,] <- c(M1,M2)
+   	comp <- comp+1}}
+  lox <- fm[,1]-dif
+  hix <- fm[,1]+dif
+  loy <- fm[,2]+dif
+  hiy <- fm[,2]-dif
+  if(is.null(main)) {if(nrow(emp)>1) {main <- "Confidence Intervals for the Pairwise Mean Comparisons"} else {main <- "Confidence Interval for the Pairwise Mean Comparison"}}  
+  main <- paste(strwrap(main,width = 0.7 * getOption("width")),collapse="\n")
+  if(is.null(ylim)) {
+  mn <- min(lox,loy,hix,hiy)-2
+  mx <- max(hix,hiy,lox,loy)+2
+  ylim<- c(mn,mx)}
+  if(is.null(col)) {
+    pc <- cbind(emp[,4] <= 0 & emp[,5] >= 0 )
+    col <- ifelse(pc,"darkred","darkblue")}
+  par(mar=c(5,5,6,5))
+  plot(NULL,bty="l",cex.lab=1.15,xlim=ylim,ylim=ylim,xlab=xlab,ylab=ylab,main=main)
+  arrows(mn,mn,mx,mx,length=0,lty=2)
+  abline(v=dm[,2],col="gray90")
+  mtext(rownames(dm),side=3,at=dm[,2])
+  abline(h=dm[,2],col="gray90")
+  mtext(rownames(dm),side=4,at=dm[,2],las=1)
+  points(fm[,1],fm[,2],pch=pch,col=col)
+  arrows(lox,loy,hix,hiy,length=0,lwd=2,col=col)  
+}
