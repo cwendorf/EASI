b5cd1be5e3199803a60d6c6d54810288f7163eb7
cwendorf <45096238+cwendorf@users.noreply.github.com>
2022-07-25 05:57:17 -0500

    Update xEDA.R

--- R/xEDA.R
+++ R/xEDA.R
@@ -21,11 +21,3 @@ plotRainclouds <- function(...,main=NULL,col="black") {
   plotDensity(...,add=TRUE,offset=.1,col=col)
   plotData(...,add=TRUE,method="jitter",offset=-.15,col=col)
 }
-
-plotCombination <- function(...,main=NULL,col="black") {
-  if(is.null(main)) main="Combination Plots"
-  plotBoxes(...,main=main,col=col)
-  plotDensity(...,add=TRUE,offset=.1,col=col)
-  plotData(...,add=TRUE,offset=.12,col=col)
-  plotFrequencies(...,add=TRUE,offset=.1,col=col)
-}
