68cc9b19902821d05549d957261202689bdb9dfc
Craig Wendorf <cwendorf@uwsp.edu>
2023-03-20 15:57:40 -0500

    Update xBoxes.R

--- R/xBoxes.R
+++ R/xBoxes.R
@@ -37,21 +37,24 @@ describeBoxes <- function(...,main=NULL,digits=3) {
     if(is.null(ylim)) {ylim <- range(pretty(c(floor(min(results)-.5),ceiling(max(results)+.5))))}
     plot(NULL,xaxs="i",yaxs="i",xaxt="n",xlim=c(.4,nrow(results)+.6),ylim=ylim,xlab=xlab,cex.lab=1.15,ylab=ylab,main=main,las=1,bty="l")
     axis(1,1:nrow(results),row.names(results))}
-  rect(1:nrow(results)+offset-.03,results[,2],1:nrow(results)+offset+.03,results[,4],border=col,lwd=2,col=.colorTransparent(col,30))
+  width <- .03*scale
+  rect(1:nrow(results)+offset-width,results[,2],1:nrow(results)+offset+width,results[,4],border=col,lwd=2,col=.colorTransparent(col,30))
   arrows(1:nrow(results)+offset,results[,1],1:nrow(results)+offset,results[,2],length=0,lwd=2,col=col)
   arrows(1:nrow(results)+offset,results[,4],1:nrow(results)+offset,results[,5],length=0,lwd=2,col=col)
-  arrows(1:nrow(results)+offset-.03,results[,3],1:nrow(results)+offset+.02,results[,3],length=0,lwd=3,col=col)
-  arrows(1:nrow(results)+offset-.03,results[,1],1:nrow(results)+offset+.02,results[,1],length=0,lwd=2,col=col)
-  arrows(1:nrow(results)+offset-.03,results[,5],1:nrow(results)+offset+.02,results[,5],length=0,lwd=2,col=col)
+  arrows(1:nrow(results)+offset-width,results[,3],1:nrow(results)+offset+width,results[,3],length=0,lwd=3,col=col)
+  arrows(1:nrow(results)+offset-width,results[,1],1:nrow(results)+offset+width,results[,1],length=0,lwd=2,col=col)
+  arrows(1:nrow(results)+offset-width,results[,5],1:nrow(results)+offset+width,results[,5],length=0,lwd=2,col=col)
   if(connect) {if(nrow(results)>1) {for (i in 1:(nrow(results)-1)) arrows(i+offset,results[i,3],i+1+offset,results[i+1,3],code=3,length=0,lty=1,col=col)}}
   if(values) {
     results <- .formatFrame(results,digits=digits)
-    text(1:nrow(results),as.numeric(results[,1]),results[,1],cex=.8,pos=pos,offset=.9,col=col)
-    text(1:nrow(results),as.numeric(results[,2]),results[,2],cex=.8,pos=pos,offset=.9,col=col)
-    text(1:nrow(results),as.numeric(results[,3]),results[,3],cex=.8,pos=pos,offset=.9,font=2,col=col)
-    text(1:nrow(results),as.numeric(results[,4]),results[,4],cex=.8,pos=pos,offset=.9,col=col)
-    text(1:nrow(results),as.numeric(results[,5]),results[,5],cex=.8,pos=pos,offset=.9,col=col)}
+    tweak <- .5+(scale-1)/4
+    text(1:nrow(results)+offset,as.numeric(results[,1]),results[,1],cex=.8,pos=pos,offset=tweak,col=col)
+    text(1:nrow(results)+offset,as.numeric(results[,2]),results[,2],cex=.8,pos=pos,offset=tweak,col=col)
+    text(1:nrow(results)+offset,as.numeric(results[,3]),results[,3],cex=.8,pos=pos,offset=tweak,font=2,col=col)
+    text(1:nrow(results)+offset,as.numeric(results[,4]),results[,4],cex=.8,pos=pos,offset=tweak,col=col)
+    text(1:nrow(results)+offset,as.numeric(results[,5]),results[,5],cex=.8,pos=pos,offset=tweak,col=col)}
 }
+
 
 plotBoxes <- function(x,...) 
   UseMethod("plotBoxes")
