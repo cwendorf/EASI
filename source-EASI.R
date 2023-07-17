a384e5ef74e823646d0126e6d420147af057b7db
Craig Wendorf <cwendorf@uwsp.edu>
2023-07-17 09:58:14 -0500

    Update source-EASI.R

--- source-EASI.R
+++ source-EASI.R
@@ -1,19 +1,17 @@
-# Name: source.github.R
-# Purpose: Source all functions from a GitHub repo
-# Author: Craig Wendorf
-# Date: 2021-8-17
+# Purpose: Source all R functions from a GitHub repo
+# Updated: 2023-07-17
 
-source.github <- function(username,repo,branch) {
-  url <- paste("https://github.com/", username, "/", repo, "/archive/refs/heads/", branch, ".zip", sep = "")
+source.github <- function(username, repo, branch) {
+  url <- paste("https://github.com/", username, "/", repo, "/archive/refs/heads/", branch, ".zip", sep="")
   name <- paste(repo, "-", branch, sep="")
-  file <- paste(name,  ".zip", sep = "")
-  download.file(url=url,destfile=file)
+  file <- paste(name,  ".zip", sep="")
+  download.file(url=url, destfile=file)
   unzip(zipfile=file)
   folder <- paste(name, "/R/", sep="")
-  paths <- list.files(path=folder,full.names=TRUE)
+  paths <- list.files(path=folder, full.names=TRUE)
   for (i in 1:length(paths)) source(paths[i])
   unlink(file)
-  unlink(name, recursive = TRUE)
+  unlink(name, recursive=TRUE)
 }
 
 # Call for this Repo
