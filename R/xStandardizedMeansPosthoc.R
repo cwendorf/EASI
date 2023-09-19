34e2174a50b99e3ba2e4bbf6b501a6acc953664d
Craig Wendorf <cwendorf@uwsp.edu>
2023-09-19 05:15:43 -0500

    Added standardized diffograms

--- R/xStandardizedMeansPosthoc.R
+++ R/xStandardizedMeansPosthoc.R
@@ -89,7 +89,13 @@ estimateStandardizedMeansPosthoc <- function(...,main=NULL,digits=3) {
 
 ### Confidence Interval Plots
 
-plotStandardizedMeansPosthoc <- function(...,main=NULL,digits=3,ylab="Standardized Mean Difference",xlab="",mu=0,line=NULL,rope=NULL,conf.level=.95,values=TRUE,pos=2,connect=FALSE,ylim=NULL,add=FALSE,pch=17,col="black",offset=0,intervals=TRUE) {
+plotStandardizedMeansPosthoc <- function(...,main=NULL,digits=3,ylab="Standardized Mean Difference",xlab="",mu=0,line=NULL,rope=NULL,conf.level=.95,values=TRUE,pos=2,connect=FALSE,ylim=NULL,add=FALSE,pch=24,col="black",offset=0,intervals=TRUE) {
   results <- estimateStandardizedMeansPosthoc(...,conf.level=conf.level,mu=mu,main=main,digits=digits)
   plotIntervals(results,add=add,main=main,xlab=xlab,ylab=ylab,ylim=ylim,values=values,line=line,rope=rope,digits=digits,connect=connect,pos=pos,pch=pch,col=col,offset=offset,intervals=intervals)
+}
+
+plotStandardizedMeansPosthocDiffogram <- function(...,main="Confidence Intervals for the Posthoc Standardized Mean Comparisons",ylab="",xlab="",conf.level=.95,ylim=NULL,pch=24,col=NULL) {
+  dm <- .describeMeans(...)
+  emp <- .estimateStandardizedMeansPosthoc(...,conf.level=conf.level)
+  .intervalsDiffogram(dm=dm,emp=emp,main=main,ylab=ylab,xlab=xlab,ylim=ylim,pch=pch,col=col)
 }
