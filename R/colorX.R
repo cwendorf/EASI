9db0ddd228b08c380e6d2b0016b4d452bb19502d
Craig Wendorf <cwendorf@uwsp.edu>
2023-09-23 10:22:28 -0500

    Added color theme

--- R/colorX.R
+++ R/colorX.R
@@ -15,29 +15,12 @@
   apply(newColor,2,function(x) {hsv(h=x[1],s=x[2],v=x[3])})
 }
 
-### Color Schemes
+### Color Themes
 
-vibrantTheme <- c(vibrantred = "#C80003", vibrantblue = "#0085C8", vibrantgold = "#C8A700", vibrantgreen = "#00C800", vibrantpurple = "#6400C8")
-mutedTheme <- c(mutedred = "#8B0000", mutedblue = "#005D8B", mutedgold = "#8B7400", mutedgreen = "#008B00", mutedpurple = "#45008B")
-
-vibrantMain <- function(n) {
-    if (n==2) {hstart=.556} else {hstart=.14}
-    colors=rev(hsv(h=seq.int(hstart,1,length.out=n),s=1,v=.784,alpha=1))
-    return(colors)
-}
-
-vibrantComp <- function(n=3) {
-    colors=c(vibrantMain(n-1),"black")
-    return(colors)
-}
-
-mutedMain <- function(n) {
-    if (n==2) {hstart=.556} else {hstart=.14}
-    colors=rev(hsv(h=seq.int(hstart,1,length.out=n),s=1,v=.545,alpha=1))
-    return(colors)
-}
-
-mutedComp <- function(n=3) {
-    colors=c(mutedMain(n-1),"black")
-    return(colors)
+theme <- function(palette="muted",type="main") {
+  if(palette=="vibrant") {palette <- c(vibrantred = "#C80003", vibrantblue = "#0085C8", vibrantgold = "#C8A700", vibrantgreen = "#00C800", vibrantpurple = "#6400C8")}
+  else if(palette=="muted") {palette <- c(mutedred = "#8B0000", mutedblue = "#005D8B", mutedgold = "#8B7400", mutedgreen = "#008B00", mutedpurple = "#45008B")}
+  if(type=="comp" && length(palette)==1) {palette <- c(palette,palette)}
+  if(type=="comp") {palette <- c(palette[1:2],"black")}
+  return(palette)
 }
