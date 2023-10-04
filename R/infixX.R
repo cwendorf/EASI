51eea0677ed4be09aff2ad604100325bf081f918
Craig Wendorf <cwendorf@uwsp.edu>
2023-10-04 17:25:13 -0500

    Updated pipe operators

--- R/infixX.R
+++ R/infixX.R
@@ -1,5 +1,13 @@
 # Estimation Approach to Statistical Inference
-## Infix Operators
+## Infix and Pipe Operators
+
+### Infix Operators
+
+"%c%" <- function(lhs,rhs) {
+  lhs <- substitute(lhs)
+  rhs <- substitute(rhs)
+  do.call(c,list(lhs,rhs))
+}
 
 ### Exposition Pipe
 
@@ -11,15 +19,24 @@
 
 ### Dot Pipes
 
-"%.>%" <- function(lhs,rhs) {
+insert_dot <- function(expr) {
+  if (is.symbol(expr)) {expr <- as.call(c(expr, quote(`.`)))}
+  else if (length(expr) == 1) {expr <- as.call(c(expr[[1]], quote(`.`)))} 
+  else if (all(sapply(expr[-1], `!=`, quote(`.`)))) {expr <- as.call(c(expr[[1]], quote(`.`), as.list(expr[-1])))}
+  return(expr)
+}
+
+'%.>%' <- forwardPipe <- function(lhs,rhs) {
   . <- eval(lhs)
   rhs <- substitute(rhs)
+  rhs <- insert_dot(rhs)
   eval(rhs)
 }
 
 "%.>>%" <- function(lhs,rhs) {
   . <- eval(lhs)
   rhs <- substitute(rhs)
+  rhs <- insert_dot(rhs)
   print(eval(rhs))
   invisible(.)
 }
