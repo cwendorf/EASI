0ca1c37e5e8ee5ee8422d91ee64d4604c28ec33d
Craig Wendorf <cwendorf@uwsp.edu>
2023-10-11 17:07:58 -0500

    Rebuilt boxplots

--- R/xBoxes.R
+++ R/xBoxes.R
@@ -19,13 +19,13 @@
   rn <- results[,1]
   results <- results[[2]]
   rownames(results) <- rn
-  colnames(results) <- c("LW","LH","Mdn","UH","UW")  
+  colnames(results) <- c("LW","LH","Mdn","UH","UW")
   return(results)
 }
 
 describeBoxes <- function(...,main=NULL,digits=3) {
   results <- .describeBoxes(...)
-  if(is.null(main)) {main <- "Boxplot Statistics for the Data"}
+  if(is.null(main)) {main <- "Boxplots for the Data"}
   results <- .formatList(list(results),main=main,digits=digits)
   return(results)
 }
@@ -33,6 +33,7 @@ describeBoxes <- function(...,main=NULL,digits=3) {
 ### Plots
 
 .bp <- function(results,main,ylab,xlab,ylim,values,digits,pos,connect,add,col,offset,scale) {
+  results <- .unformatFrame(results[[1]])
   if(!add) {
     if(is.null(ylim)) {ylim <- range(pretty(c(floor(min(results)-.5),ceiling(max(results)+.5))))}
     plot(NULL,xaxs="i",yaxs="i",xaxt="n",xlim=c(.4,nrow(results)+.6),ylim=ylim,xlab=xlab,cex.lab=1.15,ylab=ylab,main=main,las=1,bty="l")
@@ -55,32 +56,19 @@ describeBoxes <- function(...,main=NULL,digits=3) {
     text(1:nrow(results)+offset,as.numeric(results[,5]),results[,5],cex=.8,pos=pos,offset=tweak,col=col)}
 }
 
-
 plotBoxes <- function(x,...) 
   UseMethod("plotBoxes")
 
 plotBoxes.default <- function(frame,add=FALSE,main=NULL,ylab="Outcome",xlab="",ylim=NULL,offset=0,scale=1,col="black",values=TRUE,digits=3,pos=2) {
-  data <- data.frame(frame)
-  if(ncol(data)==1) {colnames(data) <- deparse(substitute(frame))}
-  if(is.null(ylim)) {
-    z <- apply(data,2,density)
-    rm <- range(sapply(z,"[","x"))
-    ylim <- range(pretty(rm))}
-  results <- .unformatFrame(describeBoxes(data,main=main,digits=digits)[[1]])
-  if(is.null(main)) {if(nrow(results)>1) {main="Boxplots for the Variables"} else {main="Boxplot for the Variable"}}
- .bp(results,main=main,ylab=ylab,xlab=xlab,ylim=ylim,values=values,digits=digits,pos=pos,connect=FALSE,add=add,col=col,offset=offset,scale=scale)
+  results <- describeBoxes(frame,main=main,digits=digits)
+  if(is.null(main)) {main=names(results)} 
+  .bp(results,main=main,ylab=ylab,xlab=xlab,ylim=ylim,values=values,digits=digits,pos=pos,connect=FALSE,add=add,col=col,offset=offset,scale=scale)
   invisible(eval(frame))
 }
 
 plotBoxes.formula <- function(formula,add=FALSE,main=NULL,ylab="Outcome",xlab="",ylim=NULL,offset=0,scale=1,col="black",values=TRUE,digits=3,pos=2) {
-  group <- eval(formula[[3]])
-  outcome <- eval(formula[[2]])
-  if(is.null(ylim)) {
-    z <- tapply(outcome,group,density)
-    rm <- range(sapply(z,"[","x"))
-    ylim <- range(pretty(rm))}
-  results <- .unformatFrame(describeBoxes(formula=formula,main=main,digits=digits)[[1]])
-  if(is.null(main)) {if(nrow(results)>1) {main="Boxplots for the Groups"} else {main="Boxplot for the Group"}}  
- .bp(results,main=main,ylab=ylab,xlab=xlab,ylim=ylim,values=values,digits=digits,pos=pos,connect=FALSE,add=add,col=col,offset=offset,scale=scale)
+  results <- describeBoxes(formula,main=main,digits=digits)
+  if(is.null(main)) {main=names(results)} 
+  .bp(results,main=main,ylab=ylab,xlab=xlab,ylim=ylim,values=values,digits=digits,pos=pos,connect=FALSE,add=add,col=col,offset=offset,scale=scale)
   invisible(eval(formula))
 }
