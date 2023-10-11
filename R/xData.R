d9f51e0ee0e39ebe2da80ba3a472054ae5cdda62
Craig Wendorf <cwendorf@uwsp.edu>
2023-10-11 18:41:10 -0500

    Rebuilt data plot

--- R/xData.R
+++ R/xData.R
@@ -1,39 +1,6 @@
 # Estimation Approach to Statistical Inference
 ## Data Plots
 
-### Data Plot
-
-plotData <- function(x,...)
-  UseMethod("plotData")
-
-plotData.default <- function(frame,add=FALSE,main=NULL,ylim=NULL,ylab="Outcome",xlab="",offset=.13,method="stack",jitter=.05,col="black",pch=16,lty="solid",connect=FALSE,...) {
-  data <- data.frame(frame)
-  if(ncol(data)==1) {colnames(data) <- deparse(substitute(frame))}
-  loc <- (1:length(data))+offset
-  if(!add) {
-    if(is.null(main)) {main="Data for the Variables"}
-    results <- describePercentiles(data)
-    results[[1]] <- results[[1]][,c(1,5)]
-    .plotMain(results,main=main,ylab=ylab,xlab=xlab,ylim=ylim)}
-  dataChart(data,add=TRUE,at=loc,vertical=TRUE,method=method,jitter=jitter,col=.colorTransparent(col,70),pch=pch)
-  if(connect && method!="jitter") {
-    lesscol <- length(data)-1
-    for(i in 1:lesscol) {segments(loc[i],data[,i],loc[i+1],data[,i+1],col=.colorTransparent(col,70),lty=lty)}}
-  invisible(eval(frame))
-}
-
-plotData.formula <- function(formula,add=FALSE,main=NULL,ylim=NULL,ylab=NULL,xlab="",offset=.13,method="stack",jitter=.05,col="black",pch=16,...) {
-  data <- unstack(model.frame(formula))
-  loc <- (1:length(data))+offset
-  if(!add) {
-    if(is.null(ylab)) {ylab <- all.vars(formula)[1]}
-    results <- describePercentiles(data)
-    results[[1]] <- results[[1]][,c(1,5)]
-    if(is.null(main)) {main="Data for the Groups"}
-    .plotMain(results,main=main,ylab=ylab,xlab=xlab,ylim=ylim)}
-  dataChart(data,add=TRUE,at=loc,vertical=TRUE,method=method,jitter=jitter,col=.colorTransparent(col,70),pch=pch)
-  invisible(eval(formula))
-}
 
 ### Modified stripchart
 
@@ -121,9 +88,7 @@ function(x, method = "overplot", jitter = 0.1, offset = 1/3, vertical = FALSE,
     invisible()
 }
 
-dataChart.formula <-
-    function(x, data = NULL, dlab = NULL, ..., subset, na.action = NULL)
-{
+dataChart.formula <- function(x, data = NULL, dlab = NULL, ..., subset, na.action = NULL) {
     if(missing(x) || (length(x) != 3L))
 	stop("formula missing or incorrect")
     m <- match.call(expand.dots = FALSE)
@@ -139,4 +104,35 @@ dataChart.formula <-
     response <- attr(attr(mf, "terms"), "response")
     if (is.null(dlab)) dlab <- names(mf)[response]
     dataChart(split(mf[[response]], mf[-response]), dlab = dlab, ...)
+}
+
+### Data Plot
+
+plotData <- function(x,...)
+  UseMethod("plotData")
+
+plotData.default <- function(frame,add=FALSE,main=NULL,ylim=NULL,ylab="Outcome",xlab="",offset=.13,method="stack",jitter=.05,col="black",pch=16,lty="solid",connect=FALSE,...) {
+  data <- data.frame(frame)
+  if(ncol(data)==1) {colnames(data) <- deparse(substitute(frame))}
+  loc <- (1:length(data))+offset
+  if(!add) {
+    if(is.null(main)) {main="Data for the Variables"}
+    results <- describePercentiles(data)
+    .plotMain(results,main=main,ylab=ylab,xlab=xlab,ylim=ylim)}
+  dataChart(data,add=TRUE,at=loc,vertical=TRUE,method=method,jitter=jitter,col=.colorTransparent(col,70),pch=pch)
+  if(connect && method!="jitter") {
+    lesscol <- length(data)-1
+    for(i in 1:lesscol) {segments(loc[i],data[,i],loc[i+1],data[,i+1],col=.colorTransparent(col,70),lty=lty)}}
+  invisible(eval(frame))
+}
+
+plotData.formula <- function(formula,add=FALSE,main=NULL,ylim=NULL,ylab="Outcome",xlab="",offset=.13,method="stack",jitter=.05,col="black",pch=16,...) {
+  if(!add) {
+    results <- describePercentiles(formula)
+    if(is.null(main)) {main="Data for the Groups"}
+    .plotMain(results,main=main,ylab=ylab,xlab=xlab,ylim=ylim)}
+  data <- unstack(model.frame(formula))
+  loc <- (1:length(data))+offset
+  dataChart(data,add=TRUE,at=loc,vertical=TRUE,method=method,jitter=jitter,col=.colorTransparent(col,70),pch=pch)
+  invisible(eval(formula))
 }
