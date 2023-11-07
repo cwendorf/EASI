bfc14c1e7e210ec90c14ea210e68bb912ba66074
Craig Wendorf <cwendorf@uwsp.edu>
2023-11-07 10:05:03 -0600

    Update xSummary.R

--- R/xSummary.R
+++ R/xSummary.R
@@ -129,7 +129,7 @@ describeSummary <- describeMeans <- function(..., main = NULL, digits = 3) {
   }
 }
 
-plotSummary <- plotSummary <- function(x, ...) {
+plotSummary <- function(x, ...) {
   UseMethod("plotSummary")
 }
 
